ad0e5899cd45b500decb9cac3fda143273533441
VitorAguiar <vitor.aguiar@me.com>
2017-06-30 12:12:23 -0300

    uncomment lines

--- geuvadis_reanalysis/expression/kallisto/index/kallisto_indexer_ref.sh
+++ geuvadis_reanalysis/expression/kallisto/index/kallisto_indexer_ref.sh
@@ -3,7 +3,7 @@
 kallisto=/home/vitor/kallisto_linux-v0.43.1/kallisto
 
 transcripts_chr=gencode.v26.CHR.transcripts
-#transcripts_all=gencode.v26.ALL.transcripts
+transcripts_all=gencode.v26.ALL.transcripts
 
 kallisto index -i $transcripts_chr.idx $transcripts_chr.fa
-#kallisto index -i $transcripts_all.idx $transcripts_all.fa
+kallisto index -i $transcripts_all.idx $transcripts_all.fa
