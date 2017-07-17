213a2ffdb77a661154d8fd9228f474d55a535fad
VitorAguiar <vitor.aguiar@me.com>
2017-07-17 13:02:24 -0300

    add scripts

---
+++ geuvadis_reanalysis/data/previous_qtls/1-download_data.sh
@@ -0,0 +1,4 @@
+wget -O battle_eqtls.xls http://genome.cshlp.org/content/suppl/2013/11/04/gr.155192.113.DC1/Supplemental_Data_1.xls
+wget -O geuvadis_eur_eqtls.txt.gz http://www.ebi.ac.uk/arrayexpress/files/E-GEUV-1/EUR373.gene.cis.FDR5.best.rs137.txt.gz
+wget -O geuvadis_eur_all_eqtls.txt.gz http://www.ebi.ac.uk/arrayexpress/files/E-GEUV-1/EUR373.gene.cis.FDR5.all.rs137.txt.gz
+wget -O barreiro_eqtls.xlsx http://www.cell.com/cms/attachment/2073022834/2068642455/mmc4.xlsx
