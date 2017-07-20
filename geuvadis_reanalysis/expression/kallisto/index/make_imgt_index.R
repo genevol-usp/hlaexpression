8728d67a395b90849df89f6f5e0c6e9d5685ed17
VitorAguiar <vitor.aguiar@me.com>
2017-07-20 08:18:17 -0300

    load tidyverse

--- geuvadis_reanalysis/expression/kallisto/index/make_imgt_index.R
+++ geuvadis_reanalysis/expression/kallisto/index/make_imgt_index.R
@@ -1,4 +1,5 @@
 devtools::load_all("/home/vitor/hlatools")
+library(tidyrverse)
 
 doMC::registerDoMC(40)
 
@@ -10,34 +11,34 @@ make_index <- function(loci, infer_missing = TRUE) {
 
   if (infer_missing) {
     alignments <- 
-      purrr::map_df(nuc_paths, ~hla_read_alignment(., omit = "N")) %>%
-      dplyr::mutate(locus = sub("^([^*]+).+$", "\\1", allele)) %>%
-      dplyr::select(locus, allele, cds) %>%
-      dplyr::group_by(locus) %>%
-      dplyr::filter(!all(grepl("\\*", cds))) %>%
-      dplyr::ungroup() %>%
+      map_df(nuc_paths, ~hla_read_alignment(., omit = "N")) %>%
+      mutate(locus = sub("^([^*]+).+$", "\\1", allele)) %>%
+      select(locus, allele, cds) %>%
+      group_by(locus) %>%
+      filter(!all(grepl("\\*", cds))) %>%
+      ungroup() %>%
       split(.$locus) %>%
-      purrr::map_df(~hla_infer(., .parallel = TRUE)) %>%
-      dplyr::select(-locus)
+      map_df(~hla_infer(., .parallel = TRUE)) %>%
+      select(-locus)
   } else {
     alignments <-
       nuc_paths %>%
-      purrr::map_df(~hla_read_alignment(., omit = "N", rm_incomplete = TRUE))
+      map_df(~hla_read_alignment(., omit = "N", rm_incomplete = TRUE))
   }
 
   alignments %>%
-  dplyr::mutate(allele3f = ifelse(grepl("_s\\d+$", allele), allele, 
-				  hla_trimnames(allele, 3)),
-		cds = hla_format_sequence(cds)) %>%
-  dplyr::group_by(cds) %>%
-  dplyr::summarize(allele = paste(allele, collapse = "/"), 
-		   allele3f = paste(unique(allele3f), collapse = "/")) %>%
-  dplyr::group_by(allele3f) %>%
-  dplyr::mutate(n = n()) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(allele = ifelse(n > 1L, allele, allele3f)) %>%
-  dplyr::select(allele, cds) %>%
-  dplyr::arrange(allele)
+  mutate(allele3f = ifelse(grepl("_s\\d+$", allele), allele, 
+         		  hla_trimnames(allele, 3)),
+         cds = hla_format_sequence(cds)) %>%
+  group_by(cds) %>%
+  summarize(allele = paste(allele, collapse = "/"), 
+            allele3f = paste(unique(allele3f), collapse = "/")) %>%
+  group_by(allele3f) %>%
+  mutate(n = n()) %>%
+  ungroup() %>%
+  mutate(allele = ifelse(n > 1L, allele, allele3f)) %>%
+  select(allele, cds) %>%
+  arrange(allele)
 }
 
 # ------------------------------------------------------------------------------
@@ -47,15 +48,15 @@ main_loci <- c("A", "B", "C", "DQA1", "DQB1", "DRB")
 other_loci <-
   list.files("/home/vitor/IMGTHLA/alignments/", pattern = "_nuc\\.txt") %>% 
   strsplit("_") %>% 
-  purrr::map_chr(1) %>%
+  map_chr(1) %>%
   .[! . %in% c(main_loci, "ClassI", "ClassII")]
 
 index <- 
-  dplyr::bind_rows(make_index(main_loci, infer_missing = TRUE),
-		   make_index(other_loci, infer_missing = FALSE)) %>%
-  dplyr::mutate(allele = paste0("IMGT_", allele)) %>%
+  bind_rows(make_index(main_loci, infer_missing = TRUE),
+            make_index(other_loci, infer_missing = FALSE)) %>%
+  mutate(allele = paste0("IMGT_", allele)) %>%
   split(.$allele) %>% 
-  purrr::map("cds")
+  map("cds")
 
 indexDNA <- Biostrings::DNAStringSet(unlist(index))
 
