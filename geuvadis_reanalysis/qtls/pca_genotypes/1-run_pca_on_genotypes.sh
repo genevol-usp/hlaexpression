c9142c0db04f22ba528c1d38247af4f2a23a72b6
VitorAguiar <vitor.aguiar@me.com>
2017-07-23 15:48:46 -0300

    fix paths

--- geuvadis_reanalysis/qtls/pca_genotypes/1-run_pca_on_genotypes.sh
+++ geuvadis_reanalysis/qtls/pca_genotypes/1-run_pca_on_genotypes.sh
@@ -2,8 +2,8 @@
 
 QTLtools=/home/vitor/QTLtools/QTLtools_1.1_Ubuntu16.04_x86_64
 
-VCF=./genotypes/eur_maf05.vcf.gz
-OUT=./pca/eur
-LOG=./pca/eur.log
+VCF=../genotypes/eur_maf05.vcf.gz
+OUT=./eur
+LOG=./eur.log
 
 $QTLtools pca --vcf $VCF --distance 6000 --center --scale --out $OUT --log $LOG
--- geuvadis_reanalysis/qtls/pca_genotypes/2-make_covariates_genotypes.R
+++ geuvadis_reanalysis/qtls/pca_genotypes/2-make_covariates_genotypes.R
@@ -1,8 +1,8 @@
 library(data.table)
 
-pcs <- fread("./pca/eur.pca", nrows = 3)
+pcs <- fread("./eur.pca", nrows = 3)
 pcs[, SampleID := sub("^.+(PC\\d+)$", "\\1", SampleID)]
 
 setnames(pcs, "SampleID", "id")
 
-fwrite(pcs, "./pca_genotypes/covariates_genos.txt", sep = "\t", quote = FALSE)
+fwrite(pcs, "./covariates_genos.txt", sep = "\t", quote = FALSE)
