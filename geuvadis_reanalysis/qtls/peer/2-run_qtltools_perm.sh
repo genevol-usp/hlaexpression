3f290da9adc6d532444196864bd821f14b2afd89
VitorAguiar <vitor.aguiar@me.com>
2017-07-25 06:53:57 -0300

    fix variable chunks in for loop

--- geuvadis_reanalysis/qtls/peer/2-run_qtltools_perm.sh
+++ geuvadis_reanalysis/qtls/peer/2-run_qtltools_perm.sh
@@ -4,31 +4,31 @@ QTLtools_dir=/home/vitor/QTLtools
 QTLtools=$QTLtools_dir/QTLtools_1.1_Ubuntu16.04_x86_64
 parallel=/home/vitor/parallel
 
-JOBS=60
+CHUNKS=100
 SAMPLES=../genotypes/samples.eur
 VCF=../genotypes/eur_maf05.vcf.gz
 COV=../pca_genotypes/covariates_genos.txt
 CMD_FILE=./cmd.txt
 
-for k in $(seq 0 5 20; seq 20 10 100)
+for k in $(seq 0 5 20; seq 30 10 100)
 do
   BED=./phenotypes/phenotypes_eur_$k.bed.gz
   OUT=./permutations/permutations_$k
   LOG=./log/k$k.log
 
-  for j in $(seq 1 $JOBS)
+  for j in $(seq 1 $CHUNKS)
   do
     echo $QTLtools cis --vcf $VCF --bed $BED --cov $COV \
-      --include-samples $SAMPLES --normal --chunk $j $JOBS --permute 1000 \
+      --include-samples $SAMPLES --normal --chunk $j $CHUNKS --permute 1000 \
       --out $OUT\_$j.txt --log $LOG
   done
 done>$CMD_FILE
 
-$parallel --gnu -j $JOBS :::: $CMD_FILE
+$parallel --gnu -j 64 :::: $CMD_FILE
 
 rm $CMD_FILE
 
-for k in $(seq 0 5 20; seq 20 10 100)
+for k in $(seq 0 5 20; seq 30 10 100)
 do
   OUT=./permutations/permutations_$k
   cat $OUT\_*.txt | gzip -c > $OUT.txt.gz
