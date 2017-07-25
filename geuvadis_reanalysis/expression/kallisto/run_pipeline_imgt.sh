fdfc5d9d02ae06f559fd4db6879cb297e6f51360
VitorAguiar <vitor.aguiar@me.com>
2017-07-25 05:40:11 -0300

    minor edits

--- geuvadis_reanalysis/expression/kallisto/run_pipeline_imgt.sh
+++ geuvadis_reanalysis/expression/kallisto/run_pipeline_imgt.sh
@@ -2,15 +2,16 @@
 
 JOBS=60
 parallel=/home/vitor/parallel
+samples=../../data/sample_info/samples_phase3_ena.txt
+write_custom_index=./index/write_genotyped_alleles.R 
 
-#$parallel --gnu -j $JOBS ./kallisto_quant_round1.sh {} :::: samples_phase3_ena.txt
+$parallel --gnu -j $JOBS ./kallisto_quant_round1.sh {} :::: $samples
 
-#Rscript process_kallisto.R 1
+Rscript process_kallisto.R 1
 
-Rscript ./index/write_genotyped_alleles.R\
-  ./quantifications_1/processed_quant.tsv ./index/sample_indices
+Rscript $write_custom_index ./quantifications_1/processed_quant.tsv ./index/sample_indices
 
-$parallel --gnu -j $JOBS ./kallisto_quant_round2.sh {} :::: samples_phase3_ena.txt
+$parallel --gnu -j $JOBS ./kallisto_quant_round2.sh {} :::: $samples
 
 Rscript process_kallisto.R 2
 
--- geuvadis_reanalysis/expression/kallisto/run_pipeline_ref.sh
+++ geuvadis_reanalysis/expression/kallisto/run_pipeline_ref.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-parallel=/home/vitor/parallel
-
 JOBS=30
+parallel=/home/vitor/parallel
+samples=../../data/sample_info/samples_phase3_ena.txt
 
-$parallel --gnu -j $JOBS ./kallisto_quant_CHR.sh {} :::: samples_phase3_ena.txt
+$parallel --gnu -j $JOBS ./kallisto_quant_CHR.sh {} :::: $samples
 
-#Rscript process_kallisto.R CHR
+Rscript process_kallisto.R CHR
 
-#$parallel --gnu -j $JOBS ./kallisto_quant_ALL.sh {} :::: samples_phase3_ena.txt
+$parallel --gnu -j $JOBS ./kallisto_quant_ALL.sh {} :::: $samples
 
-#Rscript process_kallisto.R ALL
+Rscript process_kallisto.R ALL
