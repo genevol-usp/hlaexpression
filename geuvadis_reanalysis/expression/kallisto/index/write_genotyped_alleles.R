cf12ab6884bb41daae879e0c55e6da89127f45f0
VitorAguiar <vitor.aguiar@me.com>
2017-07-27 18:16:08 -0300

    if counts < 5 write only first allele

--- geuvadis_reanalysis/expression/kallisto/index/write_genotyped_alleles.R
+++ geuvadis_reanalysis/expression/kallisto/index/write_genotyped_alleles.R
@@ -8,10 +8,14 @@ index <- readDNAStringSet("/home/vitor/hlaexpression/geuvadis_reanalysis/express
 
 genos <- 
   read_tsv(processed_quants) %>%
-  filter(est_counts > 0) %>%
-  select(subject, allele) %>%
+  select(subject, locus, allele, est_counts) %>%
   distinct() %>%
-  separate_rows(allele, sep = "=") %>%  
+  separate_rows(allele, sep = "=") %>% 
+  arrange(subject, locus, desc(est_counts)) %>%
+  group_by(subject, locus) %>%
+  filter(est_counts >= 5L | allele == first(allele)) %>%
+  ungroup() %>%
+  select(subject, allele) %>%
   mutate(path = file.path(outdir, paste0("hla_", subject, ".fa"))) %>%
   split(.$subject)
 
