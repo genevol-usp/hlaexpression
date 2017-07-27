a832da57624f3d32493037d6bafedb60cc5372e2
VitorAguiar <vitor.aguiar@me.com>
2017-07-27 19:03:01 -0300

    reformulate code but no change in results

--- geuvadis_reanalysis/expression/kallisto/make_kallisto_matrix.R
+++ geuvadis_reanalysis/expression/kallisto/make_kallisto_matrix.R
@@ -1,9 +1,14 @@
-devtools::load_all("~/hlaseqlib")
+devtools::load_all("/home/vitor/hlaseqlib")
 library(data.table)
 
 setDTthreads(36)
+setDT(geuvadis_info)
+setDT(gencode_chr_tx)
+setDT(gencode_chr_gene)
 
-samples_dt <- setDT(geuvadis_info)[kgp_phase3 == 1, .(name, subject = ena_id)]
+gencode <- gencode_chr_tx[chr %in% 1:22, .(target_id = tx_id, gene_id, gene_name)]
+
+samples_dt <- geuvadis_info[kgp_phase3 == 1, .(name, subject = ena_id)]
 
 abundance_files <- 
   file.path("./quantifications_2", samples_dt$subject, "abundance.tsv")
@@ -16,20 +21,21 @@ expression_dt <-
 	  )[samples_dt, on = .(subject)
 	  ][, .(subject = name, target_id, tpm)]
 
-gencode_autosomes <- 
-  setDT(gencode_chr_tx)[chr %in% 1:22, .(target_id = tx_id, gene_id)]
+target_set <- expression_dt[, .(target_id = unique(target_id))]
 
-imgt_ids <- 
-  expression_dt[grepl("^IMGT", target_id)
-	      ][, .(target_id = unique(target_id))
-	      ][, gene_id := imgt_to_gid(target_id)
-	      ][order(target_id)]
+autosomes_set <- 
+  target_set[gencode, on = .(target_id), nomatch = 0L][, .(target_id, gene_id)]
 
-gene_set <- rbind(gencode_autosomes, imgt_ids)[!is.na(gene_id)]
+imgt_set <-
+  target_set[grepl("^IMGT", target_id)
+	   ][, gene_name := imgt_to_gname(target_id)
+	   ][gencode, on = .(gene_name), nomatch = 0L
+	   ][order(target_id), .(target_id, gene_id)]
 
-gene_dt <- 
-  expression_dt[gene_set, on = .(target_id), nomatch = 0L
-	      ][, .(gene_tpm = sum(tpm)), by = .(subject, gene_id)]
+gene_set <- rbind(autosomes_set, imgt_set)
+
+gene_dt <- expression_dt[gene_set, on = .(target_id), nomatch = 0L
+		       ][, .(gene_tpm = sum(tpm)), by = .(subject, gene_id)]
 
 expressedGenes <- gene_dt[, .(mean(gene_tpm > 0)), by = .(gene_id)][V1 >= 0.9]
 
