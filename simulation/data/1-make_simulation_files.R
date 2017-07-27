c8d32eeb7cbfa796a41b504932e8d8290ede8899
VitorAguiar <vitor.aguiar@me.com>
2017-07-27 17:42:19 -0300

    reorganize code between the 2 scripts

--- simulation/data/1-make_simulation_files.R
+++ simulation/data/1-make_simulation_files.R
@@ -1,29 +1,52 @@
 library(data.table)
 library(Biostrings)
 devtools::load_all("/home/vitor/hlaseqlib")
+  
+setDT(geuvadis_info)
 
-geuvadis_dt <- 
-  setDT(geuvadis_info)[kgp_phase3 == 1, .(sample_id = name, subject = ena_id)]
+set.seed(2)
+
+sample_dt <- 
+  geuvadis_info[kgp_phase3 == 1, .(sample_id = name, subject = ena_id)
+	      ][sample_id %in% sample(sample_id, 50)
+	      ][order(sample_id)
+	      ][, code := sprintf("sample_%02d", 1:50)]
+
+writeLines(geuvadis_dt$sample_id, "./samples.txt")
 
 abundance_files <- 
   file.path("../../geuvadis_reanalysis/expression/kallisto/quantifications_2", 
-	    geuvadis_dt$subject, "abundance.tsv")
-names(abundance_files) <- geuvadis_dt$subject
+	    sample_dt$subject, "abundance.tsv")
+names(abundance_files) <- sample_dt$subject
 
 abundances <- 
   parallel::mclapply(abundance_files, function(i) fread(i, select = c(1, 4, 5)),
-		     mc.cores = 50)
+		     mc.cores = 25)
 
-abundances_dt <- 
-  rbindlist(abundances, idcol = "subject"
-	  )[geuvadis_dt, on = "subject"]
+abundances_dt <- rbindlist(abundances, idcol = "subject")[sample_dt, on = "subject"]
 
-abundances_wide <- 
-  dcast(abundances_dt, target_id ~ sample_id, value.var = "est_counts")
- 
+genos <- abundances_dt[grepl("^IMGT_(A|B|C|DQA1|DQB1|DRB1)", target_id) & est_counts > 0]
+genos[, `:=`(locus = imgt_to_gname(target_id), 
+	     allele = hla_trimnames(gsub("IMGT_", "", target_id), 3))]
+hom <- copy(genos)[, n := .N, by = .(subject, locus)][n == 1L][, n := NULL]
+genos <- rbind(genos, hom)[, .(subject = code, locus, allele)][order(subject, locus, allele)] 
+
+fwrite(genos, "./genos.tsv", sep = "\t", quote = FALSE)
+
+index <- readDNAStringSet("../../geuvadis_reanalysis/expression/kallisto/index/gencode.v25.CHR.IMGT.transcripts.fa")
+index <- index [width(index) >= 75]
+
+abundances_wide <- dcast(abundances_dt, target_id ~ sample_id, value.var = "est_counts")
+
+tx <- intersect(names(index), abundances_wide$target_id)
+
+index <- index[tx]
+abundances_wide <- abundances_wide[target_id %in% tx]
+
 samples <- names(abundances_wide)[-1]
 
-abundances_wide[, (samples) := lapply(.SD, function(x) round(ifelse(is.na(x), 0, x))),
-		.SDcols = samples]
+abundances_wide[, (samples) := lapply(.SD, function(x) round(ifelse(is.na(x), 0, x))), .SDcols = samples]
+abundances_wide[, (samples) := lapply(.SD, function(x) round(x/sum(x) * 3e7)), .SDcols = samples]
 
+writeXStringSet(index, "./polyester_index.fa")
 fwrite(abundances_wide, "./phenotypes.tsv", sep = "\t", quote = FALSE)
--- simulation/data/2-polyester.R
+++ simulation/data/2-polyester.R
@@ -1,57 +1,7 @@
-library(data.table)
 library(polyester)
-library(Biostrings)
-devtools::load_all("/home/vitor/hlaseqlib")
 
-index <- readDNAStringSet("../../geuvadis_reanalysis/expression/kallisto/index/gencode.v25.CHR.IMGT.transcripts.fa")
-index <- index[width(index) >= 75]
+counts_matrix <- data.matrix(data.table::fread("./phenotypes.tsv", drop = 1))
 
-ground_truth <- fread("./phenotypes.tsv")
-
-set.seed(2)
-all_samples <- names(ground_truth)[-1]
-chosen_samples <- sort(sample(all_samples, 50))
-
-samples_dt <- data.table(subject = chosen_samples,
-			 code = sprintf("sample_%02d", 1:50))
-
-writeLines(chosen_samples, "./samples.txt")
-
-counts_dt <- ground_truth[, c("target_id", (chosen_samples)), with = FALSE]
-setkey(counts_dt, target_id)
-
-genos <- 
-  melt(counts_dt[grepl("IMGT_(A|B|C|DQA1|DQB1|DRB1)", target_id)], 
-       id = 1, measure = 2:ncol(counts_dt), variable = "subject"
-      )[value > 0
-      ][, locus := imgt_to_gname(target_id)
-      ][, allele := hla_trimnames(gsub("IMGT_", "", target_id), 3)
-      ][, .(subject, locus, allele)]
-
-hom <- copy(genos)[, n := .N, by = .(subject, locus)][n == 1][, n := NULL]
-
-genos <- 
-  rbind(genos, hom)[order(subject, locus, allele)
-		  ][samples_dt, on = .(subject)
-		  ][, .(subject = code, locus, allele)]
-
-fwrite(genos, "./genos.tsv", sep = "\t", quote = FALSE)
-
-tx <- intersect(names(index), counts_dt$target_id)
-
-index <- index[tx]
-
-counts_dt <- 
-  counts_dt[tx
-	  ][, (chosen_samples) := lapply(.SD, function(x) round(x/sum(x) * 3e7)), .SDcols = chosen_samples]
-
-fwrite(counts_dt, "./phenotypes_adjusted_30Mread.tsv", 
-       quote = FALSE, sep = "\t")
-
-counts_matrix <- as.matrix(counts_dt[, -1])
-
-writeXStringSet(index, "./polyester_index.fa")
-  
 simulate_experiment_countmat(fasta = "./polyester_index.fa", 
 			     readmat = counts_matrix,
 			     readlen = 75, 
