f83a5c1035c239a5b18b2f45d7adcd196d759450
VitorAguiar <vitor.aguiar@me.com>
2017-07-28 15:28:49 -0300

    uncomment

--- simulation/data/2-simulate_parallel.sh
+++ simulation/data/2-simulate_parallel.sh
@@ -2,26 +2,26 @@
 
 parallel=/home/vitor/parallel
 
-#mkdir -p ./fastq
-#mkdir -p ./fasta_{01..50}
+mkdir -p ./fastq
+mkdir -p ./fasta_{01..50}
 
-#$parallel --gnu -j 50 Rscript 2-polyester.R {} ::: {1..50} 
+$parallel --gnu -j 50 Rscript 2-polyester.R {} ::: {1..50} 
 
-#for i in {1..2}
-#do
-#  for j in {01..50}
-#  do
-#    cat ./fasta_$j/sample_01_$i.fasta |\
-#    awk -v mate=$i 'BEGIN {RS = ">" ; FS = "\n"} NR > 1 {
-#      print\
-#      "@"gensub(/;/, " ", 1, gensub(/\//, "_", 1, $1))"/"mate"\n"\
-#      $2"\n"\
-#      "+\n"\
-#      gensub(/./, ">", "g", $2)\
-#    }' | gzip -c > ./fastq/sample_$j\_$i.fq.gz &
-#  done
-#  wait
-#done
-#wait
-#
+for i in {1..2}
+do
+  for j in {01..50}
+  do
+    cat ./fasta_$j/sample_01_$i.fasta |\
+    awk -v mate=$i 'BEGIN {RS = ">" ; FS = "\n"} NR > 1 {
+      print\
+      "@"gensub(/;/, " ", 1, gensub(/\//, "_", 1, $1))"/"mate"\n"\
+      $2"\n"\
+      "+\n"\
+      gensub(/./, ">", "g", $2)\
+    }' | gzip -c > ./fastq/sample_$j\_$i.fq.gz &
+  done
+  wait
+done
+wait
+
 rm -r ./fasta_{01..50}
