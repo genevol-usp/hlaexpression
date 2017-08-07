391f605f38e80a44d51822199ee1b85bdcdeab72
VitorAguiar <vitor.aguiar@me.com>
2017-08-07 16:57:58 -0300

    write file with EUR only

--- geuvadis_reanalysis/data/sample_info/write_samples_phase3.R
+++ geuvadis_reanalysis/data/sample_info/write_samples_phase3.R
@@ -1,15 +1,23 @@
-devtools::load_all("~/genomicRutils")
+devtools::load_all("/home/vitor/hlaseqlib")
 library(tidyverse)
 
 geuvadis_samples_phase3 <-
   geuvadis_info %>%
   filter(kgp_phase3 == 1L) %>%
-  select(subject = name, ena_id, pop) %>%
-  arrange(ena_id)
+  select(name, ena_id, pop)
 
-geuvadis_samples_phase3$ena_id %>%
+geuvadis_samples_phase3 %>%
+  pull(name) %>%
+  writeLines("samples_phase3.txt")
+
+geuvadis_samples_phase3 %>%
+  pull(ena_id) %>%
+  sort() %>%
   writeLines("samples_phase3_ena.txt")
 
-geuvadis_samples_phase3$subject %>%
+geuvadis_samples_phase3 %>%
+  filter(pop != "YRI") %>%
+  pull(ena_id) %>%
   sort() %>%
-  writeLines("samples_phase3.txt")
+  writeLines("samples_phase3_ena_eur.txt")
+
