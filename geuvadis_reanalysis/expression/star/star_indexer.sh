c8b2036afce0baa4075f31aeffd520edcfc7d040
VitorAguiar <vitor.aguiar@me.com>
2017-08-31 09:11:41 -0300

    move script to more basal dir in dir tree

--- geuvadis_reanalysis/expression/star/star_indexer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STAR=/home/vitor/STAR
-
-indexDIR=./index
-fasta=../kallisto/index/gencode.v25.CHR.IMGT.transcripts.fa
-
-$STAR --runThreadN 12 --runMode genomeGenerate --genomeDir $indexDIR\
-  --genomeFastaFiles $fasta --genomeChrBinNbits 10 --genomeSAindexNbases 13
--- simulation/expression/star/star_indexer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STAR=/home/vitor/STAR
-
-indexDIR=./index
-fasta=../../../geuvadis_reanalysis/expression/kallisto/index/gencode.v25.CHR.IMGT.transcripts.fa
-
-$STAR --runThreadN 12 --runMode genomeGenerate --genomeDir $indexDIR\
-  --genomeFastaFiles $fasta --genomeChrBinNbits 10 --genomeSAindexNbases 13
