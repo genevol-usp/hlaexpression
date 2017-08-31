cbd97f1e301225ff23e9b16de7c98440073cf6e9
VitorAguiar <vitor.aguiar@me.com>
2017-08-31 12:08:37 -0300

    add script to map to genome

---
+++ geuvadis_reanalysis/expression/star/map_and_quantify_GENOME.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+STAR=/home/vitor/STAR
+salmon=/home/vitor/Salmon-0.8.2_linux_x86_64/bin/salmon
+
+sample=$1
+
+indexDIR=./indexGENOME
+fq1=../../data/fastq/$sample\_1.fastq.gz
+fq2=../../data/fastq/$sample\_2.fastq.gz
+outMap=./mappings_GENOME
+outQuant=./quantifications_GENOME
+
+mkdir -p $outMap
+mkdir -p $outQuant
+
+outPrefix=$outMap/$sample\_
+
+$STAR --runMode alignReads --runThreadN 6 --genomeDir $indexDIR\
+  --readFilesIn $fq1 $fq2 --readFilesCommand zcat\
+  --outFilterMismatchNmax 999\
+  --outFilterMismatchNoverReadLmax 0.04\
+  --outFilterMultimapScoreRange 1\
+  --outFilterMultimapNmax 10\
+  --winAnchorMultimapNmax 50\
+  --alignIntronMax 0\
+  --alignEndsType Local\
+  --outSAMunmapped Within KeepPairs\
+  --outSAMprimaryFlag AllBestScore\
+  --outSAMtype BAM SortedByCoordinate\
+  --outFileNamePrefix $outPrefix
