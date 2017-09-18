8b43350ba2e319eca5835b9d5e664c611e4c0604
VitorAguiar <vitor.aguiar@me.com>
2017-09-18 12:36:21 -0300

    add scripts

---
+++ geuvadis_reanalysis/expression/kallisto/kallisto_quant_PRI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kallisto=/home/vitor/kallisto_linux-v0.43.1/kallisto
+
+sample=$1
+fastqR1=../../data/fastq/$sample\_1.fastq.gz
+fastqR2=../../data/fastq/$sample\_2.fastq.gz
+index=../../../imgt_index/kallisto/gencode.v25.PRI.transcripts.idx
+outdir=./quantifications_PRI
+
+$kallisto quant -i $index -t 4 -o $outdir/$sample --bias $fastqR1 $fastqR2
+
--- geuvadis_reanalysis/expression/kallisto/kallisto_quant_round1.sh
+++ geuvadis_reanalysis/expression/kallisto/kallisto_quant_round1.sh
@@ -5,9 +5,8 @@ kallisto=/home/vitor/kallisto_linux-v0.43.1/kallisto
 sample=$1
 fastqR1=../../data/fastq/$sample\_1.fastq.gz
 fastqR2=../../data/fastq/$sample\_2.fastq.gz
-
 index=../../../imgt_index/kallisto/gencode.v25.PRI.IMGT.transcripts.idx
-
-outdir=./quantifications_1
+boutdir=./quantifications_1
 
 $kallisto quant -i $index -t 4 -o $outdir/$sample $fastqR1 $fastqR2
+
