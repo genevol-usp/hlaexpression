2ae06b9d0d910855af7854447c2df9f86ae532c4
VitorAguiar <vitor.aguiar@me.com>
2017-09-18 09:51:45 -0300

    rm files

--- geuvadis_reanalysis/qtls/qtls_star/phenotypes_chr/3-make_covariates_phenotypes.R
+++
@@ -1,8 +0,0 @@
-library(data.table)
-
-pcs_dt <- 
-  fread("./phenotypes_eur_pcs.pca", nrows = 10
-      )[, SampleID := sub("^.+(PC\\d+)$", "\\1", SampleID)]
-setnames(pcs_dt, "SampleID", "id")
-
-fwrite(pcs_dt, "./covariates/covariates_pheno_10.txt", sep = "\t", quote = FALSE)
