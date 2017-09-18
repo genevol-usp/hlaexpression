489ce3953e29f1a9ec0c8ff07bfd027da29a724e
VitorAguiar <vitor.aguiar@me.com>
2017-09-18 13:16:22 -0300

    update pbs workflow

--- geuvadis_reanalysis/qtls/qtls_star/pri/2-permutations/1-submit_permut_jobs.sh
+++ geuvadis_reanalysis/qtls/qtls_star/pri/2-permutations/1-submit_permut_jobs.sh
@@ -4,8 +4,6 @@ for PC in $(seq 0 5 20; seq 30 10 100)
 do
     for CHUNK in $(seq 1 60)
     do 
-	qsub -l nodes=1:ppn=1,mem=1gb,walltime=24:00:00 -q short -j oe\
-	    -o /home/vitor/hlaexpression/geuvadis_reanalysis/qtls/qtls_star/imgt/2-permutations/log/$PBS_JOBID.log\
-	    -v PC=$PC,CHUNK=$CHUNK 1-run_qtltools_perm.sh
+	qsub -v PC=$PC,CHUNK=$CHUNK run_qtltools_perm.pbs
     done
 done
