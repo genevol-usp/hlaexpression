3b8c504c731c462c1d2b704f8ae412a62c35b3ba
VitorAguiar <vitor.aguiar@me.com>
2017-09-18 09:53:27 -0300

    rm files

--- geuvadis_reanalysis/qtls/qtls_kallisto/pca_correction/rtc/1-download_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://www.ebi.ac.uk/gwas/api/search/downloads/alternative \
-  -O gwas_catalog_v1.0.1-associations_e86_r2016-11-28.tsv
-
-wget http://jungle.unige.ch/QTLtools_examples/hotspots_b37_hg19.bed
--- geuvadis_reanalysis/qtls/qtls_kallisto/pca_correction/rtc/2-liftover_hotspots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-liftdir=/home/vitor/liftOver
-liftover=$liftdir/liftOver
-chain=$liftdir/hg19ToHg38.over.chain
-
-$liftover hotspots_b37_hg19.bed $chain hotspots_hg38.bed unmapped_to_hg38
