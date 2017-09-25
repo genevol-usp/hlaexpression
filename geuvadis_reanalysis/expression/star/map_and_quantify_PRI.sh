1ecf7d17fa5a9b7995aca03236d5780f8c78fd64
VitorAguiar <vitor.aguiar@me.com>
2017-09-25 11:43:34 -0300

    try 4 cpus for PRI

--- geuvadis_reanalysis/expression/star/map_and_quantify_PRI.sh
+++ geuvadis_reanalysis/expression/star/map_and_quantify_PRI.sh
@@ -6,13 +6,13 @@ salmon=/home/vitor/Salmon-0.8.2_linux_x86_64/bin/salmon
 sample=$1
 
 indexDIR=../../../imgt_index/star/indexPRI
-fq1=../../data/fastq/$sample\_1.fastq.gz
-fq2=../../data/fastq/$sample\_2.fastq.gz
+fq1=../../data/fastq/${sample}_1.fastq.gz
+fq2=../../data/fastq/${sample}_2.fastq.gz
 outMap=./mappings_PRI
-outPrefix=$outMap/$sample\_
+outPrefix=$outMap/${sample}_
 outQuant=./quantifications_PRI
 
-$STAR --runMode alignReads --runThreadN 6 --genomeDir $indexDIR\
+$STAR --runMode alignReads --runThreadN 4 --genomeDir $indexDIR\
   --readFilesIn $fq1 $fq2 --readFilesCommand zcat\
   --outFilterMismatchNmax 999\
   --outFilterMismatchNoverReadLmax 0.04\
@@ -30,7 +30,7 @@ bam=${outPrefix}Aligned.out.bam
 fasta=/home/vitor/gencode_data/gencode.v25.PRI.transcripts.fa
 out=$outQuant/$sample
 
-$salmon quant -t $fasta -l IU -a $bam -o $out -p 6
+$salmon quant -t $fasta -l IU -a $bam -o $out -p 4
 
 awk 'NR==1 {print $1"\t"$4"\t"$5}' $out/quant.sf > $out/quant_imgt.sf
 
