4cd358008d31c0322436edc2299981f612488c04
VitorAguiar <vitor.aguiar@me.com>
2017-09-25 09:26:41 -0300

    minor edits

--- geuvadis_reanalysis/expression/kallisto/kallisto_quant_round2.sh
+++ geuvadis_reanalysis/expression/kallisto/kallisto_quant_round2.sh
@@ -3,8 +3,8 @@
 kallisto=/home/vitor/kallisto_linux-v0.43.1/kallisto
 
 sample=$1
-fastqR1=../../data/fastq/$sample\_1.fastq.gz
-fastqR2=../../data/fastq/$sample\_2.fastq.gz
+fastqR1=../../data/fastq/${sample}_1.fastq.gz
+fastqR2=../../data/fastq/${sample}_2.fastq.gz
 
 gencode=../../../imgt_index/gencode.v25.PRI.transcripts.noIMGT.fa
 sample_hla=./sample_indices/hla_$sample.fa
