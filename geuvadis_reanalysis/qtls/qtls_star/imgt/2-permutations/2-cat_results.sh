afb8960e6df11a6c3a76c5216d6e6ba835939acc
VitorAguiar <vitor.aguiar@me.com>
2017-09-26 08:51:53 -0300

    fix indentation

--- geuvadis_reanalysis/qtls/qtls_star/imgt/2-permutations/2-cat_results.sh
+++ geuvadis_reanalysis/qtls/qtls_star/imgt/2-permutations/2-cat_results.sh
@@ -5,9 +5,9 @@ QTLtools=$QTLtools_dir/QTLtools_1.1_Ubuntu16.04_x86_64
 
 for PC in $(seq 0 5 20; seq 30 10 100)
 do
-  OUT=./results/permutations_$PC
-  cat ${OUT}_*.txt | gzip -c > $OUT.txt.gz
-  rm ${OUT}_*.txt
+    OUT=./results/permutations_$PC
+    cat ${OUT}_*.txt | gzip -c > $OUT.txt.gz
+    rm ${OUT}_*.txt
 
-  Rscript $QTLtools_dir/script/runFDR_cis.R $OUT.txt.gz 0.05 $OUT
+    Rscript $QTLtools_dir/script/runFDR_cis.R $OUT.txt.gz 0.05 $OUT
 done
