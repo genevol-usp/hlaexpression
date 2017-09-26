95d9f97bbc8f2836350e86c206eb106ab862d856
VitorAguiar <vitor.aguiar@me.com>
2017-09-26 07:05:39 -0300

    minor edits

--- geuvadis_reanalysis/data/sample_info/calc_library_size.sh
+++ geuvadis_reanalysis/data/sample_info/calc_library_size.sh
@@ -2,6 +2,6 @@
 
 for sample in `cat ./samples_phase3_ena.txt`
 do
-  n=$(zcat ../fastq/$sample\_1.fastq.gz | grep -c ^@$sample)
+  n=$(zcat ../fastq/${sample}_1.fastq.gz | grep -c ^@$sample)
   echo $sample$'\t'$n
 done>./library_sizes.txt
