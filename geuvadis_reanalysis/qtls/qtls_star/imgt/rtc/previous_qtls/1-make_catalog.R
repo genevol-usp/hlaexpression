06cec88fea9244ca9f460ee39d48435a0c9b3404
VitorAguiar <vitor.aguiar@me.com>
2017-10-02 12:44:21 -0300

    fix paths

--- geuvadis_reanalysis/qtls/qtls_star/imgt/rtc/previous_qtls/1-make_catalog.R
+++ geuvadis_reanalysis/qtls/qtls_star/imgt/rtc/previous_qtls/1-make_catalog.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
 qtl_table <-
-  read_tsv("../../../../data/previous_qtls/previous_eQTLs.tsv") %>%
+  read_tsv("../../../../../data/previous_qtls/previous_eQTL.tsv") %>%
   mutate(study = paste(paste0(phenotype, ":"), source)) %>%
   select(variant, study)
 
-write_tsv(qtl_table, "./previous_eQTLs.tsv", col_names = FALSE)
+write_tsv(qtl_table, "./previous_eQTL.tsv", col_names = FALSE)
