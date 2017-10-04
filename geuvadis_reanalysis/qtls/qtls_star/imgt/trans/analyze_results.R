23b65a04979078e97569b5d769ff07ad3b758d89
VitorAguiar <vitor.aguiar@me.com>
2017-10-04 12:17:53 -0300

    add DPB1

--- geuvadis_reanalysis/qtls/qtls_star/imgt/trans/analyze_results.R
+++ geuvadis_reanalysis/qtls/qtls_star/imgt/trans/analyze_results.R
@@ -1,17 +1,19 @@
 devtools::load_all("~/hlaseqlib")
 library(tidyverse)
 
+hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
+
 gencode_hla <- gencode_chr_gene %>%
-  filter(gene_name %in% paste0("HLA-", c("A", "B", "C", "DQA1", "DQB1", "DRB1"))) %>%
-  select(gene_name, gene_id)
+    filter(gene_name %in% hla_genes) %>%
+    select(gene_name, gene_id)
 
 hits <- 
-  read_delim("./fdr_pvals.txt", delim = " ", col_types = "ciiciid--",
-	     col_names = c("gene_id", "chr_gene", "start_gene", "variant_id", 
-			   "variant_chr", "variant_pos", "nom_pval")) %>%
-  inner_join(gencode_hla, by = "gene_id") %>%
-  mutate(log10_nom_pval = -log10(nom_pval)) %>%
-  arrange(desc(log10_nom_pval)) %>%
-  select(gene_name, variant_id, variant_chr, variant_pos, log10_nom_pval) 
+    read_delim("./fdr_pvals.txt", delim = " ", col_types = "ciiciid--",
+	       col_names = c("gene_id", "chr_gene", "start_gene", "variant_id", 
+			     "variant_chr", "variant_pos", "nom_pval")) %>%
+    inner_join(gencode_hla, by = "gene_id") %>%
+    mutate(log10_nom_pval = -log10(nom_pval)) %>%
+    arrange(desc(log10_nom_pval)) %>%
+    select(gene_name, variant_id, variant_chr, variant_pos, log10_nom_pval) 
 
 write_tsv(hits, "./trans_results.tsv")
