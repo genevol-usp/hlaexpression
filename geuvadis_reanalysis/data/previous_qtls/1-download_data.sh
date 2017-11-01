0a2b6ba6613d49198fc67c6e09faeba2d95b4b73
VitorAguiar <vitor.aguiar@me.com>
2017-11-01 11:07:54 -0200

    add delaneau and gtex

--- geuvadis_reanalysis/data/previous_qtls/1-download_data.sh
+++ geuvadis_reanalysis/data/previous_qtls/1-download_data.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 wget -O battle_eqtls.xls http://genome.cshlp.org/content/suppl/2013/11/04/gr.155192.113.DC1/Supplemental_Data_1.xls
 wget -O geuvadis_eur_eqtls.txt.gz http://www.ebi.ac.uk/arrayexpress/files/E-GEUV-1/EUR373.gene.cis.FDR5.best.rs137.txt.gz
 wget -O geuvadis_eur_all_eqtls.txt.gz http://www.ebi.ac.uk/arrayexpress/files/E-GEUV-1/EUR373.gene.cis.FDR5.all.rs137.txt.gz
 wget -O barreiro_eqtls.xlsx http://www.cell.com/cms/attachment/2073022834/2068642455/mmc4.xlsx
+wget ftp://jungle.unige.ch/SGX/QTLs/LCL_eQTL.txt
+wget ftp://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606/database/data/organism_data/RsMergeArch.bcp.gz
--- geuvadis_reanalysis/data/previous_qtls/2-make_table.R
+++ geuvadis_reanalysis/data/previous_qtls/2-make_table.R
@@ -3,51 +3,74 @@ library(tidyverse)
 
 hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
 
-gencode_hla <- gencode_chr_gene %>%
+gencode_hla_v25 <- gencode_chr_gene %>%
     filter(gene_name %in% hla_genes) %>%
     select(gene_id, gene_name)
 
-gencode_hla_V12 <- 
-    get_gencode_coords("~/gencode_data/gencode.v12.annotation.gtf.gz") %>%
+gencode_hla_v12 <- 
+    "~/gencode_data/gencode.v12.annotation.gtf.gz" %>%
+    get_gencode_coords(feature = "gene") %>% 
+    filter(gene_name %in% hla_genes) %>%
+    select(gene_id, gene_name)
+
+gencode_hla_v19 <- 
+    "~/gencode_data/gencode.v19.annotation.gtf.gz" %>%
+    get_gencode_coords(feature = "gene") %>% 
     filter(gene_name %in% hla_genes) %>%
     select(gene_id, gene_name)
 
 battle <- 
     readxl::read_excel("./battle_eqtls.xls", sheet = 1) %>%
-    inner_join(gencode_hla, by = c("GENE_NAME" = "gene_name")) %>%
+    inner_join(gencode_hla_v25, by = c("GENE_NAME" = "gene_name")) %>%
     select(phenotype = GENE_NAME, variant = SNP_ID) %>%
-    mutate(variant = recode(variant, "rs9273448" = "rs1049225"),
-	   source = "Battle (2014)")
+    mutate(source = "Battle (2014)")
 
 geuvadis_eur <- 
     read_tsv("./geuvadis_eur_eqtls.txt.gz", col_names = FALSE) %>%
-    inner_join(gencode_hla_V12, by = c("X4" = "gene_id")) %>%
+    inner_join(gencode_hla_v12, by = c("X4" = "gene_id")) %>%
     select(phenotype = gene_name, variant = X1, slope = X10, pval = X12) %>%
-    mutate(variant = recode(variant, 
-			    "rs114565353" = "rs2734971",
-			    "rs137939159" = "rs9266216",
-			    "rs115899777" = "rs9265628",
-			    "rs116405062" = "rs35957722"),
-	   source = "Lappalainen (2013)") %>%
-    arrange(phenotype) 
+    mutate(source = "Lappalainen (2013)") %>%
+    left_join(rsmerge, by = c("variant" = "rsHigh")) %>%
+    mutate(variant = ifelse(is.na(rsCurrent), variant, rsCurrent)) %>%
+    select(source, phenotype, variant, slope, pval)
 
 geuvadis_eur %>%
     select(-source) %>%
     write_tsv("./geuvadis_eqtl_slope_pvals.tsv")
 
+geuvadis_eur <- select(geuvadis_eur, phenotype, variant, source)
+
 barreiro <- 
     readxl::read_excel("./barreiro_eqtls.xlsx", 1, skip = 2) %>%
     select(phenotype = external_gene_name, variant = NI_top_snp_id) %>%
-    filter(phenotype %in% gencode_hla$gene_name) %>%
+    filter(phenotype %in% gencode_hla_v25$gene_name) %>%
     mutate(source = "Nedelec (2016)")
 
 mary <- 
     tibble(phenotype = "HLA-C", 
 	   variant = c("rs2395471", "rs9264942", "rs67384697"), 
 	   source = c("Vince (2016)", "Thomas (2009)", "Kulkarni (2011)"))
- 
+
+gtex <- 
+    tibble(phenotype = c("HLA-A", "HLA-C", "HLA-DQA1", "HLA-DQB1", "HLA-DRB1"),
+	   variant = c("rs1111180", "rs1131123", "rs397709393", "rs17612907", "rs5016923"),
+	   source = "GTEx")
+
+delaneau <-
+    read_delim("./LCL_eQTL.txt", col_names = FALSE, delim = " ") %>%
+    inner_join(gencode_hla_v19, by = c("X1" = "gene_id")) %>%
+    filter(X20 <= 0.05) %>%
+    mutate(source = "Delaneau (2017)") %>%
+    select(phenotype = gene_name, variant = X8, source)
+
+rsmerge <- read_tsv("./RsMergeArch.bcp.gz", col_names = FALSE) %>%
+    select(rsHigh = X1, rsLow = X2, build_id = X3, rsCurrent = X7) %>%
+    mutate_at(vars(rsHigh, rsLow, rsCurrent), function(x) paste0("rs", x))
+
 qtls_df <-
-    bind_rows(battle, select(geuvadis_eur, -slope, -pval), barreiro, mary) %>%
+    bind_rows(battle, geuvadis_eur, barreiro, mary, gtex, delaneau) %>%
+    left_join(rsmerge, by = c("variant" = "rsHigh")) %>%
+    mutate(variant = ifelse(is.na(rsCurrent), variant, rsCurrent)) %>%
     select(source, phenotype, variant) %>%
     arrange(source, phenotype)
 
