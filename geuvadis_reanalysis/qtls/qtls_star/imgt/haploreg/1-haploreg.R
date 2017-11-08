4eb0adc7662399b6371614d5efde747d85290372
VitorAguiar <vitor.aguiar@me.com>
2017-11-08 14:06:31 -0200

    add query to regulomeDB

--- geuvadis_reanalysis/qtls/qtls_star/imgt/haploreg/1-haploreg.R
+++ geuvadis_reanalysis/qtls/qtls_star/imgt/haploreg/1-haploreg.R
@@ -2,6 +2,19 @@ devtools::load_all("/home/vitor/hlaseqlib")
 library(tidyverse)
 library(haploR)
 		      
+hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
+
+gencode_hla <- gencode_chr_gene %>%
+    filter(gene_name %in% hla_genes) %>%
+    select(gene_id, gene_name)
+
+qtls <-
+    read_qtltools("../3-conditional_analysis/conditional_60_all.txt.gz") %>%
+    filter(bwd_best == 1) %>%
+    inner_join(gencode_hla, by = c("phen_id" = "gene_id")) %>%
+    select(gene = gene_name, rsid = var_id, rank)
+
+# haploreg
 eQTL_to_df <- function(x) {
     as.character(x) %>%
     strsplit(";") %>%
@@ -24,36 +37,46 @@ eQTL_to_df <- function(x) {
 gencode_noversion <- gencode_chr_gene %>%
     mutate(gene_id = sub("\\.\\d+$", "", gene_id))
 
-hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
-
-gencode_hla <- gencode_chr_gene %>%
-    filter(gene_name %in% hla_genes) %>%
-    select(gene_id, gene_name)
-
-qtls <-
-    read_qtltools("../3-conditional_analysis/conditional_60_all.txt.gz") %>%
-    filter(bwd_best == 1) %>%
-    inner_join(gencode_hla, by = c("phen_id" = "gene_id")) %>%
-    select(gene = gene_name, rsID = var_id, rank)
-
 haploreg_query <-
-    queryHaploreg(query = qtls$variant, ldThresh = 1, ldPop = "EUR", 
+    queryHaploreg(query = qtls$rsid, ldThresh = 1, ldPop = "EUR", 
 		  epi = "vanilla", cons = "siphy", genetypes = "gencode") %>%
     filter(rsID == query_snp_rsid) %>%
-    select(rsID, pos = pos_hg38, Chromatin_States, Chromatin_Marks, DNAse, 
-	   Proteins, eQTL, gwas, grasp, Motifs, GENCODE_name, GENCODE_distance, 
-	   dbSNP_functional_annotation, Promoter_histone_marks, 
-	   Enhancer_histone_marks)
+    select(rsid = rsID, pos = pos_hg38, Chromatin_States, Chromatin_Marks, 
+	   DNAse, Proteins, eQTL, gwas, grasp, Motifs, GENCODE_name, 
+	   GENCODE_distance, dbSNP_functional_annotation, 
+	   Promoter_histone_marks, Enhancer_histone_marks)
 
 same_gene_df <- 
     haploreg_query %>%
-    left_join(qtls, by = "rsID") %>%
-    select(rsID, gene, rank, eQTL) %>% 
+    left_join(qtls, by = "rsid") %>%
+    select(rsid, gene, rank, eQTL) %>% 
     mutate(eQTL = map(eQTL, eQTL_to_df)) %>%
     unnest() %>%
     filter(gene == gene1) %>%
     select(-gene1) %>%
-    left_join(qtls, ., by = c("gene", "rank", "rsID")) %>%
+    left_join(qtls, ., by = c("gene", "rank", "rsid")) %>%
     arrange(gene, rank, desc(pvalue))
 
 write_tsv(same_gene_df, "./haploreg_results.tsv")
+
+# regulomeDB
+regDB <- queryRegulome(query = qtls$rsid, check_bad_snps = TRUE)
+
+regDB_hits <- 
+    regDB$res.table %>%
+    select(rsid, score, hits) %>%
+    separate_rows(hits, sep = ",") %>%
+    mutate(hits = trimws(hits)) %>%
+    extract(hits, c("phenotype", "info"), "([^\\|]+)\\|(.*)") %>%
+    mutate(info = gsub("^\\||\\|$", "", info)) %>%
+    group_by(rsid, score, phenotype) %>%
+    mutate(i = 1:n()) %>%
+    ungroup() %>%
+    spread(phenotype, info) %>%
+    select(rsid, score, chromatin_struct = Chromatin_Structure,
+	   motifs = Motifs, protein_binding = Protein_Binding, 
+	   qtl = Single_Nucleotides)
+
+
+
+
