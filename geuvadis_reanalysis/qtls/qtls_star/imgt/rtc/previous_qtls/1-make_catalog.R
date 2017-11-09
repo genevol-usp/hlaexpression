2d474f5e68819e409a1cc59754aeb54f4d1d9b81
VitorAguiar <vitor.aguiar@me.com>
2017-11-09 11:37:27 -0200

    mv files to old dir

--- geuvadis_reanalysis/qtls/qtls_star/imgt/rtc/previous_qtls/1-make_catalog.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-qtl_table <-
-  read_tsv("../../../../../data/previous_qtls/previous_eQTL.tsv") %>%
-  mutate(study = paste(paste0(phenotype, ":"), source)) %>%
-  select(variant, study)
-
-write_tsv(qtl_table, "./previous_eQTL.tsv", col_names = FALSE)
