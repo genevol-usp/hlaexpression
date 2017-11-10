7b16b5e9641a28bfd40bffafce2825e8d9893635
VitorAguiar <vitor.aguiar@me.com>
2017-11-10 12:38:29 -0200

    minor update

--- geuvadis_reanalysis/expression/star/phase_hla_alleles/1000G_comparison/0-write_regions.R
+++ geuvadis_reanalysis/expression/star/phase_hla_alleles/1000G_comparison/0-write_regions.R
@@ -1,11 +1,6 @@
 devtools::load_all("/home/vitor/hlaseqlib")
 library(tidyverse)
 
-hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
-
-bed <-
-  gencode_chr_gene %>%
-  filter(gene_name %in% hla_genes) %>%
-  select(chr, start, end)
+bed <- select(gencode_hla, chr, start, end)
 
 write_tsv(bed, "./hla_regions.bed", col_names = FALSE)
