ddcc465057b1ff5624eee9b8be64623438adb220
VitorAguiar <vitor.aguiar@me.com>
2017-11-13 13:22:28 -0200

    filter duplicated ids

--- geuvadis_reanalysis/data/previous_qtls/3-make_table.R
+++ geuvadis_reanalysis/data/previous_qtls/3-make_table.R
@@ -27,10 +27,6 @@ extract_regdb_genes <- function(x) {
 
 hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
 
-gencode_hla_v25 <- gencode_chr_gene %>%
-    filter(gene_name %in% hla_genes) %>%
-    select(gene_id, gene_name)
-
 gencode_hla_v19 <- "~/gencode_data/gencode.v19.annotation.gtf.gz" %>%
     get_gencode_coords(feature = "gene") %>% 
     filter(gene_name %in% hla_genes) %>%
@@ -48,19 +44,19 @@ haploreg <- read_tsv("./eqtls_v4.1.tsv.gz", col_names = FALSE) %>%
     rename(rsid = X1, info = X2) 
 
 battle <- readxl::read_excel("./battle_eqtls.xls", sheet = 1) %>%
-    inner_join(gencode_hla_v25, by = c("GENE_NAME" = "gene_name")) %>%
+    inner_join(gencode_hla, by = c("GENE_NAME" = "gene_name")) %>%
     mutate(info = paste("Battle (2014):", GENE_NAME)) %>%
     select(rsid = SNP_ID, info)
 
 barreiro <- readxl::read_excel("./barreiro_eqtls.xlsx", 1, skip = 2) %>%
-    filter(external_gene_name %in% gencode_hla_v25$gene_name) %>%
+    filter(external_gene_name %in% gencode_hla$gene_name) %>%
     mutate(info = paste("Nedelec (2016):", external_gene_name)) %>%
     select(rsid = NI_top_snp_id, info)
 
-mary <- 
-    tibble(rsid = c("rs2395471", "rs9264942", "rs67384697"), 
-	   info = paste(c("Vince (2016):", "Thomas (2009):", "Kulkarni (2011):"), 
-			"HLA-C"))
+#mary <- 
+#    tibble(rsid = c("rs2395471", "rs9264942", "rs67384697"), 
+#	   info = paste(c("Vince (2016):", "Thomas (2009):", "Kulkarni (2011):"), 
+#			"HLA-C"))
 
 delaneau <- read_delim("./LCL_eQTL.txt", col_names = FALSE, delim = " ") %>%
     inner_join(gencode_hla_v19, by = c("X1" = "gene_id")) %>%
@@ -74,10 +70,14 @@ rsmerge <- read_tsv("./RsMergeArch.bcp.gz", col_names = FALSE) %>%
 
 mhc_rsids <- readLines("./mhc_rsids_vcf.txt")
 
-qtls_df <- bind_rows(regdb_df, haploreg, battle, barreiro, mary, delaneau) %>%
+qtls_df <- bind_rows(regdb_df, haploreg, battle, barreiro, delaneau) %>%
     left_join(rsmerge, by = c("rsid" = "rsHigh")) %>%
     mutate(rsid = ifelse(is.na(rsCurrent), rsid, rsCurrent)) %>%
     select(rsid, info) %>%
-    filter(rsid %in% mhc_rsids)
+    filter(rsid %in% mhc_rsids) %>%
+    distinct(rsid, info) %>%
+    group_by(rsid) %>%
+    summarize(info = paste(info, collapse = "/")) %>%
+    ungroup()
 
 write_tsv(qtls_df, "./qtl_catalog.tsv")
