bb38a12b7a31a40a59d2490a6dbe472ae59eb535
VitorAguiar <vitor.aguiar@me.com>
2017-11-17 11:15:21 -0200

    add analysis

---
+++ geuvadis_reanalysis/qtls/qtls_star/imgt/fenrich/3-plot_results.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+fenrich_out <- 
+    read_delim("./density_tf_around_qtl.txt", col_names = FALSE, delim = " ") %>%
+    mutate(pos = (X1 + X2)/2) %>%
+    select(pos, n = X3)
+
+png("../../../plots/fenrich.png", width = 4, height = 4, units = "in", res = 200)
+ggplot(fenrich_out, aes(pos, n)) +
+    geom_line() +
+    labs(x = "Distance to eQTLs", y = "Number of annotations / kb") +
+    theme_bw()
+dev.off()
