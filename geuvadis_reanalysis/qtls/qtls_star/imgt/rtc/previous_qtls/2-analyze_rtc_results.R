bf8d606418377d22958643fee2f20ef622156dfe
VitorAguiar <vitor.aguiar@me.com>
2017-12-05 12:34:38 -0200

    run the 7 hla genes in paralle

--- geuvadis_reanalysis/qtls/qtls_star/imgt/rtc/previous_qtls/2-analyze_rtc_results.R
+++ geuvadis_reanalysis/qtls/qtls_star/imgt/rtc/previous_qtls/2-analyze_rtc_results.R
@@ -1,37 +1,37 @@
 devtools::load_all("/home/vitor/hlaseqlib")
 library(tidyverse)
 
-hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
-
-gencode_hla <- gencode_chr_gene %>%
-    filter(gene_name %in% hla_genes) %>%
-    select(gene_id, gene_name)
+gencode_hla <- select(gencode_hla, gene_id, gene_name)
 
 qtls <-
     read_qtltools("../../3-conditional_analysis/conditional_60_all.txt.gz") %>%
     filter(bwd_best == 1) %>%
     inner_join(gencode_hla, by = c("phen_id" = "gene_id")) %>%
-    select(gene = gene_name, variant = var_id, rank)
+    select(gene = gene_name, rsid = var_id, rank)
 
 previous_eqtls <-
-    read_tsv("./previous_eQTL.tsv", col_names = c("variant", "study"))
-
-rtc_previous <- 
-    read_qtltools_rtc("./rtc_results.txt") %>% 
+    "/home/vitor/hlaexpression/geuvadis_reanalysis/data/previous_qtls/qtl_catalog.tsv" %>%
+    read_tsv(col_names = c("rsid", "info")) %>%
+    separate_rows(info, sep = ";") %>%
+    separate(info, c("study", "tissue", "gene", "pvalue"), sep = "\\|")
+    
+rtc <- 
+    list.files(".", pattern = "^rtc_results") %>%
+    map_df(read_qtltools_rtc) %>%
     rename(qtl_previous = gwas_var) %>%
     inner_join(gencode_hla, by = c("gene" = "gene_id")) %>%
     select(gene = gene_name, qtl_var, qtl_previous, d_prime, rtc)
 
-qtls_rtc_previous <- 
-    left_join(qtls, rtc_previous, 
-	      by = c("gene", "variant" =  "qtl_var")) %>%
-    inner_join(previous_eqtls, by = c("qtl_previous" =  "variant")) %>%
-    filter(rtc > 0.9) %>%
-    mutate(rtc = round(rtc, 3)) %>%
-    distinct() %>%
-    group_by(gene, variant, rank, qtl_previous, d_prime, rtc) %>%
-    summarise(study = paste(study, collapse = "/")) %>%
+rtc_df <- 
+    left_join(qtls, rtc, by = c("gene", "rsid" =  "qtl_var")) %>%
+    left_join(previous_eqtls, by = c("gene", "qtl_previous" =  "rsid")) %>%
+    filter(rtc > 0.95, !is.na(study)) %>%
+    group_by(gene, rsid) %>%
+    filter(rtc == max(rtc)) %>%
+    group_by(gene, rsid, rank, qtl_previous, d_prime, rtc, study) %>%
+    summarize(tissue = paste(tissue, collapse = ",")) %>%
     ungroup() %>%
-    arrange(gene, rank, desc(rtc))
+    arrange(gene, rank, desc(rtc)) %>%
+    mutate(rtc = round(rtc, 3))
 
-write_tsv(qtls_rtc_previous, "./results.tsv")
+write_tsv(rtc_df, "./results.tsv")
