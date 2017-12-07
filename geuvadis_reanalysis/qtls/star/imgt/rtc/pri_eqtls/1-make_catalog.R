374d0903185e53c62a2a68c041c7ce9acebf85b8
VitorAguiar <vitor.aguiar@me.com>
2017-12-07 12:31:41 -0200

    minor updates

--- geuvadis_reanalysis/qtls/star/imgt/rtc/pri_eqtls/1-make_catalog.R
+++ geuvadis_reanalysis/qtls/star/imgt/rtc/pri_eqtls/1-make_catalog.R
@@ -1,16 +1,13 @@
 devtools::load_all("~/hlaseqlib")
 library(tidyverse)
 
-hla_genes <- paste0("HLA-", c("A", "B", "C", "DPB1", "DQA1", "DQB1", "DRB1"))
-
-hla_ids <- filter(gencode_chr_gene, gene_name %in% hla_genes) %>%
-    select(gene_id, gene_name)
+gencode_hla <- select(gencode_hla, gene_id, gene_name)
 
 qtl_table <-
     "../../../pri/3-conditional_analysis/conditional_60_all.txt.gz" %>%
     read_qtltools() %>%
     filter(bwd_best == 1L) %>%
-    inner_join(hla_ids, by = c("phen_id" = "gene_id")) %>%
+    inner_join(gencode_hla, by = c("phen_id" = "gene_id")) %>%
     mutate(info = paste(gene_name, "PRI", sep = ": ")) %>%
     select(var_id, info)
 
