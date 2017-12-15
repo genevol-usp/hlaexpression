19ba4b7500b78ac393019852df97caefcc22872d
VitorAguiar <vitor.aguiar@me.com>
2017-12-15 12:20:58 -0200

    use quantMode GeneCounts

--- simulation/PEreads_75bp/expression/star/map_and_quantify_GENOME.sh
+++ simulation/PEreads_75bp/expression/star/map_and_quantify_GENOME.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 STAR=/home/vitor/STAR
-salmon=/home/vitor/Salmon-0.8.2_linux_x86_64/bin/salmon
-samtools=/home/vitor/samtools-1.3.1/samtools
 
 sample=$1
 
 indexDIR=../../../../imgt_index/star/indexGENOME
-fq1=../../data/fastq/$sample\_1.fastq.gz
-fq2=../../data/fastq/$sample\_2.fastq.gz
+fq1=../../data/fastq/${sample}_1.fastq.gz
+fq2=../../data/fastq/${sample}_2.fastq.gz
 outMap=./mappings_GENOME
-outQuant=./quantifications_GENOME
-outPrefix=$outMap/$sample\_
-
-mkdir -p $outMap
-mkdir -p $outQuant
+outPrefix=${outMap}/${sample}_
 
 $STAR --runMode alignReads --runThreadN 6 --genomeDir $indexDIR\
   --readFilesIn $fq1 $fq2 --readFilesCommand zcat\
@@ -28,24 +22,6 @@ $STAR --runMode alignReads --runThreadN 6 --genomeDir $indexDIR\
   --alignEndsType Local\
   --outSAMprimaryFlag AllBestScore\
   --outSAMtype None\
-  --quantMode TranscriptomeSAM\
-  --quantTranscriptomeBan Singleend\
+  --quantMode GeneCounts\
   --outFileNamePrefix $outPrefix
 
-bam=${outPrefix}Aligned.toTranscriptome.out.bam
-fasta=/home/vitor/gencode_data/gencode.v25.PRI.transcripts.fa
-out=$outQuant/$sample
-
-$salmon quant -t $fasta -l IU -a $bam -o $out -p 6
-
-header=${outPrefix}header.sam
-imgtbam=${outPrefix}imgt.bam
-
-$samtools view -H $bam > $header
-
-$samtools view -f 0x2 -F 0x100 $bam |\
-  LC_ALL=C grep -F -f ./ids_to_filter.txt |\
-  cat $header - |\
-  $samtools view -Sb - > $imgtbam
-
-rm ${outPrefix}Aligned* ${outPrefix}Log* ${outPrefix}SJ* ${outPrefix}header.sam
