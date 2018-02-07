9ee6f01c84d210a2ed107d2470f25e6b3848bc17
VitorAguiar <vitor.aguiar@me.com>
2018-02-07 12:10:19 -0200

    deal with special cases for each locus

--- imgt_index/test_index.R
+++ imgt_index/test_index.R
@@ -1,115 +1,133 @@
 devtools::load_all("~/hlaseqlib")
-    
-make_closest_allele_df <- function(distmatrix, inc_alleles, comp_alleles) {
+library(tidyverse)
 
-    distmatrix[inc_alleles, comp_alleles] %>%
-    apply(1, function(x) which(x == min(x, na.rm = TRUE))) %>%
-    purrr::map(~tibble::tibble(closest = names(.))) %>%
-    dplyr::bind_rows(.id = "inc_allele") %>% 
-    dplyr::mutate(`1` = hla_trimnames(inc_allele, 1) == hla_trimnames(closest, 1),
-		  `2` = hla_trimnames(inc_allele, 2) == hla_trimnames(closest, 2),
-		  `3` = hla_trimnames(inc_allele, 3) == hla_trimnames(closest, 3),
-		  `4` = hla_trimnames(inc_allele, 4) == hla_trimnames(closest, 4)) %>%
-    tidyr::gather(field, value, `1`:`4`) %>%
-    dplyr::group_by(inc_allele) %>%
-    dplyr::filter(all(value == FALSE) | value == TRUE) %>%
-    dplyr::slice(which.max(field)) %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange(inc_allele) %>%
-    dplyr::select(inc_allele, closest)
+locus <- commandArgs(TRUE)[1]
+gen_file <- paste0("~/IMGTHLA/alignments/", locus, "_gen.txt")
+
+if (grepl("DRB\\d+", locus)) { 
+    locus_nuc <- "DRB"
+} else {
+    locus_nuc <- locus
 }
 
-locus <- "A"
-gen_file <- paste0("~/IMGTHLA/alignments/", locus, "_gen.txt")
-nuc_file <- paste0("~/IMGTHLA/alignments/", locus, "_nuc.txt")
+nuc_file <- paste0("~/IMGTHLA/alignments/", locus_nuc, "_nuc.txt")
 
-hla_df <- hla_read_alignment("~/IMGTHLA/alignments/A_nuc.txt") 
+hla_df <- hla_read_alignment(nuc_file) %>%
+   mutate(gene = sub("^([^*]+).+$", "\\1", allele)) %>%
+   filter(gene == locus) %>%
+   select(-gene)
 
-hla_df$cds <- stringr::str_split(hla_df$cds, "", simplify = TRUE) %>%
-    apply(1, . %>% paste(collapse = ""))
+if (all(grepl("\\*", hla_df$cds))) {
+    stop(paste("no complete sequence for locus", locus))
+}
 
-complete_alleles <- hla_df$allele[!grepl("\\*", hla_df$cds)]
-incomplete_alleles <- hla_df$allele[grepl("\\*", hla_df$cds)]
+if (nrow(hla_df) == 1L || all(!grepl("\\*", hla_df$cds))) {
+    
+    final_df <- hla_df
 
-cds_sequenced <- stringr::str_split(hla_df$cds, "", simplify = TRUE) %>%
-    apply(2, function(x) !any(x == "*"))
+} else {
 
-run <- rle(cds_sequenced)
+    distmatrix <- make_dist_matrix(hla_df)
 
-ends <- cumsum(run$lengths)
-starts <- ends - run$lengths + 1L
+    closest_allele_df <- make_closest_allele_df(distmatrix)
 
-run_df <- 
-    tibble::tibble(value = run$values, 
-		   start = starts, 
-		   end = ends) %>%
-    dplyr::filter(value == TRUE) %>%
-    dplyr::mutate(l = purrr::map2_int(start, end, ~length(.x:.y))) %>%
-    dplyr::filter(l == max(l))
+    closest_allele_df$id <- closest_allele_df %>% group_indices(inc_allele)
 
-hla_df_cds_common <- hla_df %>%
-    dplyr::mutate(cds = substring(cds, run_df$start, run_df$end))
+    closest_allele_df_step2 <-
+	bind_rows(select(closest_allele_df, id, allele = inc_allele),
+		  select(closest_allele_df, id, allele = closest)) %>%
+	distinct() %>%
+	left_join(hla_df, by = "allele") %>%
+	split(.$id) %>%
+	map(make_dist_matrix) %>%
+	map(make_closest_allele_df) %>%
+	bind_rows()
 
-cds_common <- hla_df_cds_common$cds
-names(cds_common) <- hla_df_cds_common$allele
+    closest_within_type <- closest_allele_df_step2 %>%
+	find_closest_within_type()
 
-distmatrix <- stringdist::stringdistmatrix(cds_common, cds_common,
-					   method = "hamming", 
-					   useNames = "names")
+    inferred_df <- closest_within_type %>%
+	left_join(hla_df, by = c("inc_allele" = "allele")) %>%
+	left_join(hla_df, by = c("closest" = "allele")) %>%
+	mutate(cds = purrr::map2_chr(cds.x, cds.y, hla_attribute_seq)) %>%
+	select(allele = inc_allele, cds)
 
-diag(distmatrix) <- NA
+    final_df <- hla_df %>%
+	filter(!grepl("\\*", cds)) %>%
+	bind_rows(inferred_df) %>%
+	arrange(allele)
+}
 
-min_dist_df <-
-    make_closest_allele_df(distmatrix, incomplete_alleles, complete_alleles)
+if (!file.exists(gen_file)) {
 
-inferred_df <- min_dist_df %>%
-    dplyr::left_join(hla_df, by = c("inc_allele" = "allele")) %>%
-    dplyr::left_join(hla_df, by = c("closest" = "allele")) %>%
-    dplyr::mutate(cds = purrr::map2_chr(cds.x, cds.y, hla_attribute_seq)) %>%
-    dplyr::select(allele = inc_allele, cds)
+    out_df <- final_df %>%
+	mutate(cds = hla_format_sequence(cds)) %>%
+	rename(transcript = cds) %>%
+	mutate(allele3f = hla_trimnames(allele, 3)) %>%
+	distinct(allele3f, transcript, .keep_all = TRUE) %>%
+	group_by(allele3f) %>%
+	mutate(n = n()) %>%
+	ungroup() %>%
+	mutate(allele = ifelse(n > 1L, allele, allele3f)) %>%
+	select(allele, transcript) %>%
+	arrange(allele)
 
-final_df <- hla_df %>%
-    dplyr::filter(allele %in% complete_alleles) %>%
-    dplyr::bind_rows(inferred_df) %>%
-    dplyr::arrange(allele)
+} else {
 
-utr_df <- hla_read_utr(gen_file) %>%
-    dplyr::full_join(tibble::tibble(allele = final_df$allele), by = "allele") %>%
-    dplyr::mutate_at(dplyr::vars(utr5, utr3), 
-		     ~ifelse(is.na(.), paste(rep("*", 100L), collapse = ""), .)) %>%
-    tidyr::unite(utr, utr5, utr3, sep = "|")
+    utr_df <- hla_read_utr(gen_file) %>%
+	full_join(final_df, by = "allele") %>%
+	mutate_at(vars(utr5, utr3), 
+		  ~ifelse(is.na(.), paste(rep("*", 100L), collapse = ""), .)) 
+	
+    hla_df_utr <- utr_df %>%
+	unite(cds, utr5, cds, utr3, sep = "")
 
-complete_utr_alleles <- utr_df$allele[!grepl("\\*", utr_df$utr)]
-incomplete_utr_alleles <- utr_df$allele[grepl("\\*", utr_df$utr)]
+    utr_df <- utr_df %>%
+	unite(utr, utr5, utr3, sep = "|") %>%
+	select(allele, utr)
 
-min_dist_utr_df <- 
-    make_closest_allele_df(distmatrix, incomplete_utr_alleles, complete_utr_alleles) 
+    if (all(!grepl("\\*", utr_df$utr))) {
+	
+	final_utr_df <- utr_df %>%
+	    separate(utr, c("utr5", "utr3"), sep = "\\|")
+    
+    } else {
 
-inferred_utr_df <- min_dist_utr_df %>%
-    dplyr::left_join(utr_df, by = c("inc_allele" = "allele")) %>%
-    dplyr::left_join(utr_df, by = c("closest" = "allele")) %>%
-    dplyr::mutate(utr = purrr::map2_chr(utr.x, utr.y, hla_attribute_seq)) %>%
-    dplyr::select(allele = inc_allele, utr)
+	distmatrix_utr <- make_dist_matrix(hla_df_utr)
 
-final_utr_df <- utr_df %>%
-    dplyr::filter(allele %in% complete_utr_alleles) %>%
-    dplyr::bind_rows(inferred_utr_df) %>%
-    dplyr::arrange(allele) %>%
-    tidyr::separate(utr, c("utr5", "utr3"), sep = "\\|")
+	closest_allele_utr <- make_closest_allele_df(distmatrix_utr)
 
-out_df <- 
-    dplyr::left_join(final_df, final_utr_df, by = "allele") %>%
-    dplyr::select(allele, utr5, cds, utr3) %>%
-    dplyr::mutate_at(dplyr::vars(utr5, cds, utr3), hla_format_sequence) %>%
-    dplyr::mutate(utr5 = substring(utr5, nchar(utr5)-74L, nchar(utr5)),
-		  utr3 = substring(utr3, 1, 75)) %>%
-    tidyr::unite(transcript, utr5, cds, utr3, sep = "") %>%
-    dplyr::mutate(allele3f = hla_trimnames(allele, 3)) %>%
-    dplyr::distinct(allele3f, transcript, .keep_all = TRUE) %>%
-    dplyr::group_by(allele3f) %>%
-    dplyr::mutate(n = n()) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(allele = ifelse(n > 1L, allele, allele3f)) %>%
-    dplyr::select(allele, transcript) %>%
-    dplyr::arrange(allele)
+	closest_within_type_utr <- closest_allele_utr %>%
+	    find_closest_within_type()
 
+	inferred_utr_df <- closest_within_type_utr %>%
+	    left_join(utr_df, by = c("inc_allele" = "allele")) %>% 
+	    left_join(utr_df, by = c("closest" = "allele")) %>%
+	    mutate(utr = map2_chr(utr.x, utr.y, hla_attribute_seq)) %>%
+	    select(allele = inc_allele, utr)
+
+	final_utr_df <- utr_df %>%
+	    filter(!grepl("\\*", utr)) %>%
+	    bind_rows(inferred_utr_df) %>%
+	    arrange(allele) %>%
+	    separate(utr, c("utr5", "utr3"), sep = "\\|")
+    }
+
+    out_df <- 
+	left_join(final_df, final_utr_df, by = "allele") %>%
+	select(allele, utr5, cds, utr3) %>%
+	mutate_at(vars(utr5, cds, utr3), hla_format_sequence) %>%
+	mutate(utr5 = substring(utr5, nchar(utr5)-74L, nchar(utr5)),
+	       utr3 = substring(utr3, 1, 75)) %>%
+	unite(transcript, utr5, cds, utr3, sep = "") %>%
+	mutate(allele3f = hla_trimnames(allele, 3)) %>%
+	distinct(allele3f, transcript, .keep_all = TRUE) %>%
+	group_by(allele3f) %>%
+	mutate(n = n()) %>%
+	ungroup() %>%
+	mutate(allele = ifelse(n > 1L, allele, allele3f)) %>%
+	select(allele, transcript) %>%
+	arrange(allele)
+}
+
+write_tsv(out_df, paste0("./index_", locus, ".tsv"))
