5b24f090ef395a1ffd7359e8507e8a8e7b541a12
VitorAguiar <vitor.aguiar@me.com>
2018-02-19 14:50:34 -0300

    add note

--- geuvadis_reanalysis/expression/star/phase_hla_alleles/3-write_results.R
+++ geuvadis_reanalysis/expression/star/phase_hla_alleles/3-write_results.R
@@ -5,21 +5,29 @@ expression_df <-
     read_tsv("../supplemented/quantifications_2/processed_imgt_quants.tsv") %>%
     filter(locus %in% gencode_hla$gene_name) %>%
     left_join(geuvadis_info, by = c("subject" = "ena_id")) %>%
-    mutate(allele = gsub("IMGT_", "", allele),
-	   allele_3F = hla_trimnames(allele, 3)) %>%
-    select(subject = name, locus, allele, allele_3F, tpm) %>%
+    mutate(allele = gsub("IMGT_", "", allele)) %>%
+    select(subject = name, locus, allele, tpm) %>%
     arrange(subject, locus)
 
 kgp_calls <- read_tsv("./1000G_comparison/hla_haps_phased.tsv") 
-   
+  
+
+# I removed this DQB1 allele bc:
+# This is actually a pair of alleles which share the same 3 fields but don't
+# have the same CDS, because in insertion in on of them.
+# Because in the haplotype assignment I only consider positions in the ref
+# genome, they are equal. So the individual having these alleles is homozygote
+# in this respect.
+# These alleles are probably associated to typing errors. True heterozygotes for
+# one of the alleles are called as heterozygotes with the 2 alleles, with the
+# other true allele missing. This happens because these alleles are so similar
+# that the reads of one map to the other.
+
 haps_exp_df <- kgp_calls %>%
-    filter(!grepl("/", allele)) %>%
-    left_join(expression_df, by = c("subject", "locus", "allele")) %>%
+    filter(!grepl("/", allele), !grepl("DQB1\\*05:03:01", allele)) %>%
+    left_join(expression_df, by = c("subject", "locus", "allele")) %>% 
     distinct(subject, locus, hap, allele, .keep_all = TRUE) %>%
-    left_join(expression_df, by = c("subject", "locus", "allele" = "allele_3F")) %>%
-    distinct(subject, locus, hap, allele.y, .keep_all = TRUE) %>%
-    mutate(tpm = ifelse(is.na(tpm.x), tpm.y, tpm.x)) %>%
-    select(subject, locus, hap, hla_allele = allele.y, tpm)
+    rename(hla_allele = allele)
 
 eqtl_df <- read_tsv("./best_eqtl.tsv") %>%
     left_join(gencode_hla, by = c("phen_id" = "gene_id")) %>%
@@ -39,4 +47,4 @@ haps_exp_snp_df <-
     left_join(haps_exp_df, eqtl_info, by = c("subject", "locus", "hap")) %>%
     select(subject, locus, hap, rsid, allele, hla_allele, tpm)
 
-write_tsv(out_df, "./data/1000G_haps_expression_rsid.tsv")
+write_tsv(haps_exp_snp_df, "./data/1000G_haps_expression_rsid.tsv")
