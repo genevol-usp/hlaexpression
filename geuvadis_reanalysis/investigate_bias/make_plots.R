b3961114189e93b3e5b33f284f330f6a9718616e
VitorAguiar <vitor.aguiar@me.com>
2018-02-20 11:42:20 -0300

    update plot

--- geuvadis_reanalysis/investigate_bias/make_plots.R
+++ geuvadis_reanalysis/investigate_bias/make_plots.R
@@ -5,11 +5,10 @@ df <- read_tsv("./integrated_data.tsv")
 png("./plots/expression_by_refallele.png", width = 10, height = 5, units = "in", res = 200)
 ggplot() +
     geom_jitter(data = df, aes(genotype, resid, color = dist),
-                width = .25, alpha = 1/2, size = .75) +
-    scale_color_gradient(low = "white", high = "red") +
+                width = .25, size = .75) +
+    scale_color_gradient(low = "cornflowerblue", high = "darkred") +
     geom_text(data = distinct(df, index, gene_name, rsid),
-              aes(x = 1.75, y = 3, label = rsid), size = 3, color = "white") +
+              aes(x = 1.75, y = 3, label = rsid), size = 3) +
     facet_grid(index~gene_name, scales = "free") +
-    theme_dark() +
-    labs(color = "divergence from reference")
+    labs(y = "expression", color = "divergence from reference")
 dev.off()
