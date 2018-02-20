e2ba3bc112a32df46817e9bea45e5bb6748c6fc7
VitorAguiar <vitor.aguiar@me.com>
2018-02-20 06:16:46 -0300

    keep unphased

--- geuvadis_reanalysis/expression/star/phase_hla_alleles/3-write_results.R
+++ geuvadis_reanalysis/expression/star/phase_hla_alleles/3-write_results.R
@@ -5,28 +5,34 @@ expression_df <-
     read_tsv("../supplemented/quantifications_2/processed_imgt_quants.tsv") %>%
     filter(locus %in% gencode_hla$gene_name) %>%
     left_join(geuvadis_info, by = c("subject" = "ena_id")) %>%
-    mutate(allele = gsub("IMGT_", "", allele)) %>%
-    select(subject = name, locus, allele, tpm) %>%
+    mutate(allele = gsub("IMGT_", "", allele),
+	   allele_3F = hla_trimnames(allele, 3)) %>%
+    select(subject = name, locus, allele, allele_3F, tpm) %>%
     arrange(subject, locus)
 
 kgp_calls <- read_tsv("./1000G_comparison/hla_haps_phased.tsv") 
   
 
-# I removed this DQB1 allele bc:
-# This is actually a pair of alleles which share the same 3 fields but don't
-# have the same CDS, because in insertion in on of them.
-# Because in the haplotype assignment I only consider positions in the ref
-# genome, they are equal. So the individual having these alleles is homozygote
-# in this respect.
-# These alleles are probably associated to typing errors. True heterozygotes for
-# one of the alleles are called as heterozygotes with the 2 alleles, with the
-# other true allele missing. This happens because these alleles are so similar
-# that the reads of one map to the other.
+# DQB1*05:03:01:01 and DQB1*05:03:01:03:
+# Pair of alleles which share the same 3 fields but don't have the same CDS due
+# to an insertion in on of them. Because in the haplotype assignment I only
+# consider positions in the ref genome, they are equal. So the individual having
+# these alleles is homozygote in this respect. These alleles are associated
+# with typing errors. True heterozygotes for one of these alleles are called as
+# heterozygotes with the 2 alleles, with the other true allele missing. This
+# happens because these alleles are so similar that the reads of one map to the
+# other.
 
 haps_exp_df <- kgp_calls %>%
-    filter(!grepl("/", allele), !grepl("DQB1\\*05:03:01", allele)) %>%
-    left_join(expression_df, by = c("subject", "locus", "allele")) %>% 
-    distinct(subject, locus, hap, allele, .keep_all = TRUE) %>%
+    separate_rows(allele, sep = "/") %>%
+    left_join(expression_df, by = c("subject", "locus", "allele")) %>%
+    left_join(expression_df, by = c("subject", "locus", "allele" = "allele_3F")) %>%
+    mutate(tpm = ifelse(is.na(tpm.x), tpm.y, tpm.x)) %>%
+    distinct(subject, locus, hap, allele.y, tpm) %>%
+    group_by(subject, locus, hap) %>%
+    summarise(allele = paste(allele.y, collapse = "/"),
+	      tpm = paste(tpm, collapse = "/")) %>%
+    ungroup() %>%
     rename(hla_allele = allele)
 
 eqtl_df <- read_tsv("./best_eqtl.tsv") %>%
