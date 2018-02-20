c8eddfe389b9de44a39a74ea0ef18c92d2ff7d4b
VitorAguiar <vitor.aguiar@me.com>
2018-02-20 05:35:34 -0300

    fix paths

--- geuvadis_reanalysis/qtls/star/supplemented/caveman/7-analyze_results.R
+++ geuvadis_reanalysis/qtls/star/supplemented/caveman/7-analyze_results.R
@@ -8,7 +8,7 @@ imgt_qtls <-
     select(gene_id = phen_id, gene_name, rank, var_id, pos = var_from)
 
 pri_qtls <-
-    read_qtltools("../../pri/3-conditional_analysis/conditional_60_all.txt.gz") %>%
+    read_qtltools("../../transcriptome/3-conditional_analysis/conditional_60_all.txt.gz") %>%
     inner_join(select(gencode_hla, gene_id, gene_name), by = c("phen_id" = "gene_id")) %>%
     filter(bwd_best == 1L) %>%
     select(gene_id = phen_id, gene_name, rank, var_id, pos = var_from)
@@ -26,7 +26,7 @@ results_best_imgt <- read_tsv("./results.best") %>%
     summarize(caveman_var_pos = paste(caveman_var_pos, collapse = "/")) %>%
     ungroup()
 
-results_best_pri <- read_tsv("../../pri/caveman/results.best") %>%
+results_best_pri <- read_tsv("../../transcriptome/caveman/results.best") %>%
     separate(GENE, c("gene_id", "chr", "pos", "ref", "alt"), sep = "_", convert = TRUE) %>%
     inner_join(pri_qtls, by = c("gene_id", "pos")) %>%
     select(gene_name, rank, var_id, pos, caveman_var_pos = POS, P, CaVEMaN, Probability) %>%
@@ -38,9 +38,6 @@ results_best_pri <- read_tsv("../../pri/caveman/results.best") %>%
     group_by(gene_name, rank, var_id, pos, P, CaVEMaN, Probability, best) %>%
     summarize(caveman_var_pos = paste(caveman_var_pos, collapse = "/")) %>%
     ungroup()
-
-print(results_best_imgt, n = Inf)
-print(results_best_pri, n = Inf)
 
 list(HLA_personalized = results_best_imgt, Reference = results_best_pri) %>%
     bind_rows(.id = "index") %>%
