a99473f3f46a1087ce0fa990b5da57921e313f21
VitorAguiar <vitor.aguiar@me.com>
2018-02-21 10:11:41 -0300

    update phasing analysis

--- geuvadis_reanalysis/expression/star/phase_hla_alleles/3-write_results.R
+++ geuvadis_reanalysis/expression/star/phase_hla_alleles/3-write_results.R
@@ -11,7 +11,41 @@ expression_df <-
     arrange(subject, locus)
 
 kgp_calls <- read_tsv("./1000G_comparison/hla_haps_phased.tsv") 
-  
+
+#kgp_haps <- kgp_calls %>%
+#    mutate(locus = sub("HLA-", "", locus)) %>%
+#    spread(locus, allele)
+#
+#kgp_diff <- read_tsv("./1000G_comparison/hla_diffs_to_1000Ghaps.tsv")
+#
+#allele_codes <- read_tsv("./PHASE/codes-phase.inp") %>%
+#    mutate(allele = sub("IMGT_", "", allele))
+#
+#phase_calls <-
+#    read_phase("./PHASE/phase.out", 
+#	       loci = sub("HLA-", "", gencode_hla$gene_name)) %>%
+#    rename(code = allele) %>%
+#    left_join(allele_codes, by = c("locus", "code")) %>%
+#    select(subject, locus, hap, hla_allele = allele, uncertain)
+#
+#phase_haps <- phase_calls %>%
+#    select(-uncertain) %>%
+#    spread(locus, hla_allele) %>%
+#    select(-hap)
+#
+#kgp_class1 <- select(kgp_haps, subject:C)
+#phase_class1 <- select(phase_haps, subject:C)
+#
+#diff_subjects <- anti_join(kgp_class1, phase_class1) %>%
+#    filter(!grepl("/", A), !grepl("/", B), !grepl("/", C)) %>%
+#    distinct(subject)
+#
+#s <- diff_subjects$subject[2] 
+#kgp_class1 %>% filter(subject == s)
+#phase_class1 %>% filter(subject == s)
+#kgp_diff %>% filter(subject == s, locus %in% paste0("HLA-", c("A", "B", "C")))
+
+
 
 # DQB1*05:03:01:01 and DQB1*05:03:01:03:
 # Pair of alleles which share the same 3 fields but don't have the same CDS due
--- geuvadis_reanalysis/expression/star/phase_hla_alleles/PHASE/1-make_phase_input.R
+++ geuvadis_reanalysis/expression/star/phase_hla_alleles/PHASE/1-make_phase_input.R
@@ -1,10 +1,10 @@
 devtools::load_all("/home/vitor/hlaseqlib")
 library(tidyverse)
 
-hla_genes <- sort(gencode_hla$gene_name)
+hla_genes <- gencode_hla$gene_name
 
 genotypes <- 
-    "../../imgt/quantifications_2/processed_imgt_quants.tsv" %>%
+    "../../supplemented/quantifications_2/processed_imgt_quants.tsv" %>%
     read_tsv() %>%
     filter(locus %in% hla_genes) %>%
     mutate(subject = convert_ena_ids(subject),
