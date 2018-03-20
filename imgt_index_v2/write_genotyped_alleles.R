5dce13bc3caec3891acd5150a4fa9235ae4583d4
VitorAguiar <vitor.aguiar@me.com>
2018-03-20 11:48:55 -0300

    rm any processing involving counts and mv it to scripts which generate genos

--- imgt_index_v2/write_genotyped_alleles.R
+++ imgt_index_v2/write_genotyped_alleles.R
@@ -8,15 +8,6 @@ index <-
     readDNAStringSet("/home/vitor/hlaexpression/imgt_index_v2/imgt_index.fa")
 
 genos <- read_tsv(processed_quants) %>%
-    select(subject, locus, allele, est_counts) %>%
-    group_by(subject, locus, allele) %>%
-    summarize(est_counts = sum(est_counts)) %>%
-    ungroup() %>%
-    separate_rows(allele, sep = "=") %>% 
-    arrange(subject, locus, desc(est_counts)) %>%
-    group_by(subject, locus) %>%
-    filter(est_counts >= 5L | allele == first(allele)) %>%
-    ungroup() %>%
     select(subject, allele) %>%
     mutate(path = file.path(outdir, paste0("hla_", subject, ".fa"))) %>%
     split(.$subject)
