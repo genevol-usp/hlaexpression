60445cfc70e24107abc7bb63810403b1d19cc379
VitorAguiar <vitor.aguiar@me.com>
2018-03-29 04:03:26 -0300

    use compiled qtltools

--- geuvadis_reanalysis/eqtl_mapping/transcriptomemapping/hla_personalized/1-phenotypes/2-run_pca_on_phenotypes.sh
+++ geuvadis_reanalysis/eqtl_mapping/transcriptomemapping/hla_personalized/1-phenotypes/2-run_pca_on_phenotypes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-QTLtools=/home/vitor/QTLtools/QTLtools_1.1_Ubuntu16.04_x86_64
+qtltools=/home/vitor/qtltools/bin/QTLtools
 
-$QTLtools pca --bed ./phenotypes_eur.bed.gz --out ./phenotypes_eur_pcs --center --scale
+$qtltools pca --bed ./phenotypes_eur.bed.gz --out ./phenotypes_eur_pcs --center --scale
