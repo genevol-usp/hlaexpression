9a85a40ff193763219aeab608bc461f82b9da64f
VitorAguiar <vitor.aguiar@me.com>
2018-04-02 13:02:22 -0300

    reorganize dir

--- simulation/PEreads_75bp/data/1-make_simulation_files.R
+++ simulation/PEreads_75bp/data/1-make_simulation_files.R
@@ -11,28 +11,30 @@ sample_df <- geuvadis_info %>%
     mutate(code = sprintf("sample_%02d", 1:50)) %>%
     select(subject, code)
 
+processed_imgt <- 
+    "~/hlaexpression/geuvadis_reanalysis/expression/3-map_to_transcriptome/hla_personalized/quantifications/processed_imgt_quants.tsv" %>%
+    read_tsv() %>%
+    inner_join(sample_df, by = "subject") %>%
+    select(subject = code, Name = allele, NumReads = est_counts)
+
 abundances <- 
-    file.path("~/hlaexpression/geuvadis_reanalysis/expression/star/supplemented/quantifications_2",
+    file.path("~/hlaexpression/geuvadis_reanalysis/expression/3-map_to_transcriptome/hla_personalized/quantifications",
 	      sample_df$subject, "quant.sf") %>%
     setNames(sample_df$subject) %>%
     map_df(read_tsv, .id = "subject") %>%
     left_join(sample_df, by = "subject") %>%
-    select(subject = code, Name, Length, EffectiveLength, TPM, NumReads)
+    select(subject = code, Name, NumReads) %>%
+    filter(!grepl("IMGT_", Name)) %>%
+    bind_rows(distinct(processed_imgt)) %>%
+    arrange(subject, Name)
 
-genos <- abundances %>%
-    filter(grepl("IMGT", Name)) %>%
-    mutate(locus = imgt_to_gname(Name)) %>%
+genos <- processed_imgt %>%
+    mutate(locus = imgt_to_gname(Name),
+	   allele = gsub("IMGT_", "", Name)) %>%
     filter(locus %in% gencode_hla$gene_name) %>%
-    select(subject, locus, allele = Name) %>%
-    mutate(allele = sub("IMGT_", "", allele),
-	   allele = hla_trimnames(allele, 3)) %>%
-    group_by(subject, locus) %>%
-    mutate(n = ifelse(n() == 1L, "1_1", "1")) %>%
-    ungroup() %>%
-    separate_rows(n, sep = "_") %>%
     select(subject, locus, allele) %>%
-    arrange(subject, locus, allele)
-
+    arrange(subject, locus)
+    
 write_tsv(genos, "./genos.tsv")
 
 index <- readDNAStringSet("../../../imgt_index_v2/gencode.v25.PRI.IMGT.transcripts.fa")
@@ -43,17 +45,15 @@ tx <- intersect(names(index), unique(abundances$Name))
 abundances_df_intersect <- abundances %>%
     filter(Name %in% tx) %>%
     group_by(subject) %>%
-    mutate(TrueCounts = as.integer(round(NumReads/sum(NumReads) * 3e7)),
-	   TrueTPM = counts_to_tpm(TrueCounts, EffectiveLength)) %>%
+    mutate(TrueCounts = as.integer(round(NumReads/sum(NumReads) * 3e7))) %>%
     ungroup()
 
-phenotypes_tpm_counts <- abundances_df_intersect %>%
-    select(subject, Name, TrueCounts, TrueTPM)
+phenotype_counts <- abundances_df_intersect %>%
+    select(subject, Name, TrueCounts)
 
-write_tsv(phenotypes_tpm_counts, "./phenotypes_counts_tpm.tsv")
+write_tsv(phenotype_counts, "./phenotypes_trueCounts.tsv")
 
-phenotypes <- phenotypes_tpm_counts %>%
-    select(-TrueTPM) %>%
+phenotypes <- phenotype_counts %>%
     spread(subject, TrueCounts) %>%
     mutate_at(vars(sample_01:sample_50), ~ifelse(is.na(.), 0L, .))
 
