75c938fee729ae80189128a44164e52ab41c4ff5
VitorAguiar <vitor.aguiar@me.com>
2019-01-15 14:47:26 -0200

    add results

---
+++ geuvadis_reanalysis/eqtl_mapping/transcriptomemapping/hla_personalized/rtc/top_previous_qtls/write_validated_variants.R
@@ -0,0 +1,45 @@
+devtools::load_all("/home/vitor/Libraries/hlaseqlib")
+library(tidyverse)
+
+gencode_hla <- select(gencode_hla, gene_name, gene_id)
+
+qtls <- read_tsv("../../2-conditional_analysis/hla_qtls_all.tsv") 
+
+best_qtls <- qtls %>%
+    filter(best == 1) %>%
+    select(gene, rank, qtl_hlapers = var_id)
+
+catalog <-
+    "~/hlaexpression/geuvadis_reanalysis/data/previous_qtls/top_qtl_catalog.tsv" %>%
+    read_tsv(col_names = FALSE) %>%
+    separate_rows(X2, sep = ";") %>%
+    separate(X2, c("study", "gene", "pvalue"), sep = "\\|") %>%
+    mutate(pvalue = round(as.numeric(pvalue), 2)) %>%
+    select(variant = X1, gene, study, pvalue) %>%
+    filter(study %in% c("petersdorf2015", "kulkarni2011", "vince2017", "xl9_raj2016"))
+
+ref_vars_status <- catalog %>%
+    select(-pvalue) %>%
+    inner_join(qtls, by = c("gene", "variant" = "var_id")) %>%
+    filter(signif == 1) %>%
+    select(validated_var = variant, gene, study, rank, 
+	   validated_var_pval = pval, validated_var_signif = signif)
+
+rtc <- read_qtltools_rtc("./rtc_results.txt") %>%
+    inner_join(gencode_hla, by = c("gene" = "gene_id")) %>%
+    select(gene = gene_name, qtl_var, qtl_ref = gwas_var, r_squared, d_prime, rtc)
+
+qtls_rtc <-
+    left_join(best_qtls, rtc, by = c("gene", "qtl_hlapers" = "qtl_var")) %>%
+    inner_join(catalog, by = c("qtl_ref" =  "variant"), 
+	      suffix = c("_hlapers", "_ref")) %>% 
+    filter(gene_hlapers == gene_ref)
+
+out <- qtls_rtc %>%
+    select(gene = gene_hlapers, rank, qtl = qtl_hlapers, 
+	   validated_var = qtl_ref, study, r_squared, d_prime, rtc) %>%
+    left_join(ref_vars_status, by = c("gene", "rank", "validated_var", "study")) %>%
+    mutate_at(vars(r_squared:validated_var_pval), ~round(., 2))
+
+
+write_tsv(out, "./validated_variants_results.tsv")
