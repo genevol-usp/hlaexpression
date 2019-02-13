379b3e2d120eb9f0f234017df4ee28dea3e9f243
VitorAguiar <vitor.aguiar@me.com>
2019-02-13 15:12:26 -0200

    update catalog

--- geuvadis_reanalysis/data/previous_qtls/2-write_catalog.R
+++ geuvadis_reanalysis/data/previous_qtls/2-write_catalog.R
@@ -21,7 +21,8 @@ geuvadis_gene <- read_tsv("./EUR373.gene.cis.FDR5.all.rs137.txt.gz") %>%
     group_by(GENE_ID) %>%
     mutate(min_rank = rank(pvalue, ties = "min")) %>%
     ungroup() %>%
-    select(gene_name, rsid = SNP_ID, pval = log10pvalue, min_rank) %>%
+    mutate(study = "GeuvadisGene") %>%
+    select(study, gene_name, rsid = SNP_ID, pval = log10pvalue, min_rank) %>%
     arrange(gene_name, min_rank)
 
 geuvadis_exon <- read_tsv("./EUR373.exon.cis.FDR5.all.rs137.txt.gz") %>%
@@ -29,7 +30,8 @@ geuvadis_exon <- read_tsv("./EUR373.exon.cis.FDR5.all.rs137.txt.gz") %>%
     group_by(GENE_ID) %>%
     mutate(min_rank = rank(pvalue, ties = "min")) %>%
     ungroup() %>%
-    select(gene_name, rsid = SNP_ID, pval = log10pvalue, min_rank) %>%
+    mutate(study = "GeuvadisExon") %>%
+    select(study, gene_name, rsid = SNP_ID, pval = log10pvalue, min_rank) %>%
     arrange(gene_name, min_rank)
 
 gtex_gene <-
@@ -40,66 +42,73 @@ gtex_gene <-
 	   mutate(min_rank = rank(`P-Value`, ties = "min"),
 		  pval = -log10(`P-Value`)) %>%
 	   select(gene_name = `Gene Symbol`, rsid = `SNP Id`, pval, min_rank)) %>%
+    mutate(study = "GTExV7") %>%
+    select(study, everything()) %>%
     arrange(gene_name, min_rank)
     
-delaneau2018 <- read_delim("./delaneau_LCL_eqtls.txt", col_names = FALSE, delim = " ") %>%
+delaneau2017 <- read_delim("./delaneau_LCL_eqtls.txt", col_names = FALSE, delim = " ") %>%
     inner_join(gencode_hla_v19, by = c("X1" = "gene_id")) %>%
     filter(X20 <= 0.05) %>%
     group_by(X1) %>%
     mutate(min_rank = rank(X20, ties = "min")) %>%
     ungroup() %>%
-    mutate(pval = -log10(X20)) %>%
-    select(gene_name, rsid = X8, pval, min_rank)
+    mutate(study = "Delaneau2017",
+	   pval = -log10(X20)) %>%
+    select(study, gene_name, rsid = X8, pval, min_rank)
 
-vince2016 <- tibble(gene_name = "HLA-C",
-		rsid = "rs2395471",
-		pval = NA,
-		min_rank = NA)
+fairfax2012 <- readxl::read_excel("./ng.2205-S2.xls", 3) %>%
+    filter(Gene %in% gencode_hla_v19$gene_name) %>%
+    select(gene_name = Gene, rsid = SNP, pval = P) %>%
+    group_by(gene_name) %>%
+    mutate(min_rank = rank(pval, ties = "min")) %>%
+    ungroup() %>%
+    bind_rows(tibble(gene_name = "HLA-C", rsid = "rs10484554", 
+		     pval = 5.2e-16, min_rank = 1)) %>%
+    mutate(study = "Fairfax2012") %>%
+    select(study, everything()) %>%
+    arrange(gene_name, min_rank)
 
-thomas2009 <- tibble(gene_name = "HLA-C",
+
+vince2016 <- tibble(study = "Vince2016",
+		    gene_name = "HLA-C",
+		    rsid = "rs2395471",
+		    pval = NA,
+		    min_rank = NA)
+
+thomas2009 <- tibble(study = "Thomas2009",
+		     gene_name = "HLA-C",
 		     rsid = "rs9264942",
 		     pval = NA,
 		     min_rank = NA)
 
-thomas2012 <- tibble(gene_name = "HLA-DPB1",
+thomas2012 <- tibble(study = "Thomas2012",
+		     gene_name = "HLA-DPB1",
 		     rsid = "rs9277534",
 		     pval = NA,
 		     min_rank = NA)
 
-kulkarni2011 <- tibble(gene_name = "HLA-C",
+kulkarni2011 <- tibble(study = "Kulkarni2011",
+		       gene_name = "HLA-C",
 		       rsid = "rs67384697",
 		       pval = NA,
 		       min_rank = NA)
 
-petersdorf2015 <- tibble(gene_name = c("HLA-DPB1"),
-			 rsid = c("rs2281389"), 
-			 pval = NA,
-			 min_rank = NA)
+raj2016 <- tibble(study = "Raj2016",
+		  gene_name = c("HLA-DQA1", "HLA-DQB1", "HLA-DRB1"),
+		  rsid = "rs9271593",
+		  pval = NA,
+		  min_rank = NA)
 
-raj2016 <- tibble(gene_name = c("HLA-DQA1", "HLA-DQB1", "HLA-DRB1"),
-		      rsid = "rs9271593",
-		      pval = NA,
-		      min_rank = NA)
-
-ou2019 <- tibble(gene_name = "HLA-DPA1",
+ou2019 <- tibble(study = "Ou2019",
+		 gene_name = "HLA-DPA1",
 		 rsid = "rs3077",
 		 pval = NA,
 		 min_rank = NA)
 
-
 integrated_data <-
-    list(geuvadis_gene = geuvadis_gene,
-	 geuvadis_exon = geuvadis_exon,
-	 gtex_v7 = gtex_gene,
-	 delaneau2018 = delaneau2018,
-	 thomas2009 = thomas2009,
-	 thomas2012 = thomas2012,
-	 kulkarni2011 = kulkarni2011,
-	 vince2016 = vince2016,
-	 petersdorf2015 = petersdorf2015,
-	 raj2016 = raj2016,
-	 ou2019 = ou2019) %>%
-    bind_rows(.id = "study") %>%
+    bind_rows(geuvadis_gene, geuvadis_exon, gtex_gene, delaneau2017, 
+	      fairfax2012, thomas2009, thomas2012, kulkarni2011, vince2016, 
+	      raj2016, ou2019) %>%
     convert_rsIDs()
 
 eqtl_catalog <- integrated_data %>%
