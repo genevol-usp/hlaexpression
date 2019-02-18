6c9efae5c1649df98b0b1e06f865fb78deb2f371
VitorAguiar <vitor.aguiar@me.com>
2019-02-18 19:32:10 -0300

    update

--- supplementary_tables/tabS5.R
+++ supplementary_tables/tabS5.R
@@ -2,6 +2,6 @@ library(tidyverse)
 
 read_tsv("../geuvadis_reanalysis/eqtl_mapping/transcriptomemapping/hla_personalized/rtc/crd/results.tsv") %>%
     select(gene, rank, eqtl = variant, crd_eqtl = crd_var, d_prime, rtc) %>%
-    filter(rtc >= 0.95) %>%
+    filter(rtc >= 0.9) %>%
     mutate_at(vars(d_prime, rtc), ~round(., 2)) %>%
     write_csv("./tables/S5_table.csv")
