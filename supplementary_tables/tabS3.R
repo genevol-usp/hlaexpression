71e463ac44dfd2f387a30687df3ec2fc2658a48c
VitorAguiar <vitor.aguiar@me.com>
2019-02-18 19:32:10 -0300

    update

--- supplementary_tables/tabS3.R
+++ supplementary_tables/tabS3.R
@@ -1,14 +1,13 @@
 library(tidyverse)
 
-link_df <- tribble(~study, ~link,
-		   "GeuvadisExon", "https://doi.org/10.1038/nature12531",
-		   "GeuvadisGene", "https://doi.org/10.1038/nature12531",
-		   "GTExV7", "https://doi.org/10.1038/nature24277",
-		   "Fairfax2012", "https://doi.org/10.1038/ng.2205")
-
 read_tsv("../geuvadis_reanalysis/eqtl_mapping/transcriptomemapping/hla_personalized/rtc/top_previous_qtls/results.tsv") %>%
     select(gene, rank, eqtl_hlapers = qtl_personalized, 
 	   eqtl_previous = qtl_previous, d_prime, rtc, info) %>%
     extract(info, "study", "(.+) .") %>% 
-    left_join(link_df) %>%
+    mutate(eqtl_previous = paste0(eqtl_previous, " (", study, ")"),
+	   d_prime = round(d_prime, 2),
+	   rtc = round(rtc, 2),
+	   significant = as.integer(rtc > 0.9)) %>%
+    select(-study) %>%
+    arrange(gene, rank, desc(rtc)) %>%
     write_csv("./tables/S3_table.csv")
