a13844b2e6f6dca962a17a5f4a6ffed6f3104a06
VitorAguiar <vitor.aguiar@me.com>
2019-02-20 10:43:28 -0300

    update

--- supplementary_tables/tabS4.R
+++ supplementary_tables/tabS4.R
@@ -1,4 +1,5 @@
 library(tidyverse)
 
 read_tsv("../geuvadis_reanalysis/eqtl_mapping/transcriptomemapping/hla_personalized/functional_features/results.tsv") %>%
+    mutate_at(vars(tfbs, dhs, histone_marks), ~ifelse(is.na(.), "", .)) %>%
     write_csv("./tables/S4_table.csv")
--- supplementary_tables/tabS6.R
+++ supplementary_tables/tabS6.R
@@ -1,4 +1,5 @@
 library(tidyverse)
 
 read_tsv("../geuvadis_reanalysis/eqtl_mapping/transcriptomemapping/hla_personalized/rtc/gwas/results.tsv") %>%
+    mutate(`trait (GWAS variant)` = ifelse(`trait (GWAS variant)` == "NA (NA)", "",  `trait (GWAS variant)`)) %>%
     write_csv("./tables/S6_table.csv")
